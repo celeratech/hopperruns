5e9450ca6aaa1198cce97e4a9f09b37190bd0e99
Hirshol Pheir <hirshol@gmail.com>
2024-03-09 20:54:24 +0000

    hopper run LOOPaugmentstepdown0_20240309.20.54.23

--- LOOPaugmentstepdown0/verilog5/fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP.v
+++ LOOPaugmentstepdown0/verilog5/fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP.v
@@ -3,8 +3,8 @@
 //NAME:fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP
 //GENERATOR REVISION:0.3.7
 //FET TYPE:n
-//ON RESISTANCE:0.100 Ohms
-//VDS RATING:30V
+//ON RESISTANCE:0.050 Ohms
+//VDS RATING:6V
 //VGS RATING:6V
 //BODY DIODE:yes
 //DIODE DRIVE:diode
@@ -16,21 +16,23 @@
 //Celera Confidential Do Not Copy fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet.v
 //Celera:fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet
 //Celera Confidential Symbol Generator
-//power NMOS:Ron:0.100 Ohm
-//Vgs 6V Vds 30V
+//power NMOS:Ron:0.050 Ohm
+//Vgs 6V Vds 6V
 //Kelvin:no
 
-module fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet (GATE,SOURCE,DRAIN,SUB);
+module fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet (GATE,SOURCE,DRAIN,NMOSiso6,SUB);
 input GATE;
 inout SOURCE;
 inout DRAIN;
 input SUB;
+input NMOSiso6;
 endmodule
 
 
 //Celera Confidential Do Not Copy NMOS_REPLICA
-module an5g30dw2_33p4x0p2x2p0x1p0 (DRAIN,GATE,SOURCE,SUB);
+module an5g6dw1_34p0x0p2x2p0x1p0 (DRAIN,GATE,SOURCE,ISO,SUB);
 input GATE;
+input ISO;
 input SUB;
 inout SOURCE;
 inout DRAIN;
@@ -48,17 +50,18 @@ endmodule
 
 //Celera Confidential Do Not Copy fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP
 //Celera Confidential Symbol Generator
-//power NMOS:Ron:0.100 Ohm
-//Vgs 6V Vds 30V
+//power NMOS:Ron:0.050 Ohm
+//Vgs 6V Vds 6V
 //Kelvin:yes
 
-module fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP (GATE,SOURCE,DRAIN,SOURCEk,DRAINk,IREPLICA,SUB);
+module fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP (GATE,SOURCE,DRAIN,NMOSiso6,SOURCEk,DRAINk,IREPLICA,SUB);
 input GATE;
 inout SOURCE;
 inout DRAIN;
 inout DRAINk;
 inout SOURCEk;
 input SUB;
+input NMOSiso6;
 inout IREPLICA;
 
 //Celera Confidential Do Not Copy fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet
@@ -66,18 +69,20 @@ fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet XnmosPower0(
 .DRAIN (DRAIN),
 .GATE (GATE),
 .SOURCE (SOURCE),
+.NMOSiso6 (NMOSiso6),
 .SUB (SUB)
 );
 //,diesize,fet_fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWZREP_Xfet
 
-//Celera Confidential Do Not Copy an5g30dw2_33p4x0p2x2p0x1p0
-an5g30dw2_33p4x0p2x2p0x1p0 XnmosreplicaREP_0(
+//Celera Confidential Do Not Copy an5g6dw1_34p0x0p2x2p0x1p0
+an5g6dw1_34p0x0p2x2p0x1p0 XnmosreplicaREP_0(
 .DRAIN (DRAIN),
 .GATE (GATE),
 .SOURCE (IREPLICA),
+.ISO (NMOSiso6),
 .SUB (SUB)
 );
-//,diesize,an5g30dw2_33p4x0p2x2p0x1p0
+//,diesize,an5g6dw1_34p0x0p2x2p0x1p0
 
 //Celera Confidential Do Not Copy PEBBLElink
 PEBBLElink XlinkD(
