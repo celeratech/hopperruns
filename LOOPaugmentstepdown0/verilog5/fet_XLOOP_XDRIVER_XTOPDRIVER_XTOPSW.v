22896d79826e6fc7541f36dd07a2cfc620857079
Hirshol Pheir <hirshol@gmail.com>
2024-03-09 19:33:50 +0000

    hopper run LOOPaugmentstepdown0_20240309.19.33.50

--- LOOPaugmentstepdown0/verilog5/fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW.v
+++ LOOPaugmentstepdown0/verilog5/fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW.v
@@ -3,8 +3,8 @@
 //NAME:fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW
 //GENERATOR REVISION:0.3.7
 //FET TYPE:n
-//ON RESISTANCE:0.100 Ohms
-//VDS RATING:6V
+//ON RESISTANCE:0.400 Ohms
+//VDS RATING:30V
 //VGS RATING:6V
 //BODY DIODE:yes
 //DIODE DRIVE:diode
@@ -16,38 +16,35 @@
 //Celera Confidential Do Not Copy fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet.v
 //Celera:fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet
 //Celera Confidential Symbol Generator
-//power NMOS:Ron:0.100 Ohm
-//Vgs 6V Vds 6V
+//power NMOS:Ron:0.400 Ohm
+//Vgs 6V Vds 30V
 //Kelvin:no
 
-module fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet (GATE,SOURCE,DRAIN,NMOSiso6,SUB);
+module fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet (GATE,SOURCE,DRAIN,SUB);
 input GATE;
 inout SOURCE;
 inout DRAIN;
 input SUB;
-input NMOSiso6;
 endmodule
 
 
 //Celera Confidential Do Not Copy fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW
 //Celera Confidential Symbol Generator
-//power NMOS:Ron:0.100 Ohm
-//Vgs 6V Vds 6V
+//power NMOS:Ron:0.400 Ohm
+//Vgs 6V Vds 30V
 //Kelvin:no
 
-module fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW (GATE,SOURCE,DRAIN,NMOSiso6,SUB);
+module fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW (GATE,SOURCE,DRAIN,SUB);
 input GATE;
 inout SOURCE;
 inout DRAIN;
 input SUB;
-input NMOSiso6;
 
 //Celera Confidential Do Not Copy fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet
 fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet XnmosPower0(
 .DRAIN (DRAIN),
 .GATE (GATE),
 .SOURCE (SOURCE),
-.NMOSiso6 (NMOSiso6),
 .SUB (SUB)
 );
 //,diesize,fet_fet_XLOOP_XDRIVER_XTOPDRIVER_XTOPSW_Xfet
