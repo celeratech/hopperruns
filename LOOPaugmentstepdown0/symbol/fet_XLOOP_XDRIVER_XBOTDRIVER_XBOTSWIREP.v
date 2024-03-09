c5fe15cc239781d62d4011bd332af55a1dfc15ba
Hirshol Pheir <hirshol@gmail.com>
2024-03-09 16:47:19 +0000

    hopper run LOOPaugmentstepdown0_20240309.16.47.19

--- LOOPaugmentstepdown0/symbol/fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWIREP.v
+++ LOOPaugmentstepdown0/symbol/fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWIREP.v
@@ -1,16 +1,17 @@
 //Celera:fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWIREP
 //Celera Confidential Symbol Generator
-//power NMOS:Ron:0.100 Ohm
-//Vgs 6V Vds 30V
+//power NMOS:Ron:0.050 Ohm
+//Vgs 6V Vds 6V
 //Kelvin:yes
 
-module fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWIREP (GATE,SOURCE,DRAIN,SOURCEk,DRAINk,IREPLICA,SUB);
+module fet_XLOOP_XDRIVER_XBOTDRIVER_XBOTSWIREP (GATE,SOURCE,DRAIN,NMOSiso6,SOURCEk,DRAINk,IREPLICA,SUB);
 input GATE;
 inout SOURCE;
 inout DRAIN;
 inout DRAINk;
 inout SOURCEk;
 input SUB;
+input NMOSiso6;
 inout IREPLICA;
 endmodule
 
