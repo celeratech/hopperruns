0f316c946bbafd6ecd133140e3387f930b84a11a
Hirshol Pheir <hirshol@gmail.com>
2024-10-09 12:16:00 +0000

    hopper run celera_amp_20241009.12.16.00

---
+++ celera_amp/symbol/resistor_amplifier_8cbdbecf_XresRF.v
@@ -0,0 +1,11 @@
+//Celera:resistor_amplifier_8cbdbecf_XresRF
+//Celera Confidential Symbol Generator
+//RESISTOR:460.00KOhm TYPE:poly DFT:no
+module resistor_amplifier_8cbdbecf_XresRF (RP,
+CELG,
+RN);
+inout RP;
+inout RN;
+input CELG;
+endmodule
+
---
+++ celera_amp/verilog7/FORCE_celera_amp.v
@@ -0,0 +1,19 @@
+// ------------------------ Module Definitions -----------
+// ------------------------ Module Verilog ---------------
+module FORCE_celera_amp (GND, INN, INP, OUT, SIMPV, ok_amplifier, enable_amplifier, register_ok_amplifier_8de7f9e7_Xd_ok_amplifier, register_enable_amplifier_e456c5d3_Xd_enable_amplifier);
+  input  GND;
+  input  INN;
+  input  INP;
+  input  OUT;
+  input  SIMPV;
+output  ok_amplifier;
+output  enable_amplifier;
+input  register_ok_amplifier_8de7f9e7_Xd_ok_amplifier;
+input  register_enable_amplifier_e456c5d3_Xd_enable_amplifier;
+
+
+// ------------------------ Wires ------------------------
+
+// ------------------------ Networks ---------------------
+endmodule
+
