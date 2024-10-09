cbadfe4b5c56630abe028c24607cd0bf0417c3e2
Hirshol Pheir <hirshol@gmail.com>
2024-10-09 21:52:01 +0000

    hopper run delayClock301_20241009.21.52.01

---
+++ delayClock301/symbol/delayclock_ce78f020.v
@@ -0,0 +1,13 @@
+//Celera:delayclock_ce78f020
+//TYPE:clock  EDGE:fall DFT:no ACC:no%
+module delayclock_ce78f020 (in,CELV,out,clock,celeraporb,
+CELG,CELSUB);
+input CELV;
+input in;
+output out;
+input clock;
+input celeraporb;
+input CELSUB;
+input CELG;
+endmodule
+
