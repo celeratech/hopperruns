85118cb49dcc138f64a48027e9f9502dbc38442a
Hirshol Pheir <hirshol@gmail.com>
2024-10-12 23:35:42 +0000

    hopper run resistor_ptype_accu_20241012.23.35.42

---
+++ resistor_ptype_accu/verilog7/FORCE_resistor_ptype_accu.v
@@ -0,0 +1,23 @@
+// ------------------------ Module Definitions -----------
+// ------------------------ Module Verilog ---------------
+module FORCE_resistor_ptype_accu (RN, GND, RTN, RP2V, RP6V, RP12V, RP20V, RP24V, RP30V, RP36V, RP40V, RP45V, RP60V);
+  input  RN;
+  input  GND;
+  input  RTN;
+  input  RP2V;
+  input  RP6V;
+  input  RP12V;
+  input  RP20V;
+  input  RP24V;
+  input  RP30V;
+  input  RP36V;
+  input  RP40V;
+  input  RP45V;
+  input  RP60V;
+
+
+// ------------------------ Wires ------------------------
+
+// ------------------------ Networks ---------------------
+endmodule
+
---
+++ resistor_ptype_accu/verilog7/resistor_ptype_accu.v
@@ -0,0 +1,92 @@
+// ------------------------ Module Definitions -----------
+module resistor_4fe26a31 ();
+endmodule
+
+module resistor_1c79009d ();
+endmodule
+
+module resistor_8659784c ();
+endmodule
+
+module resistor_0eca3039 ();
+endmodule
+
+module resistor_54fdf25c ();
+endmodule
+
+module resistor_3f7b6fd7 ();
+endmodule
+
+module resistor_982d13a7 ();
+endmodule
+
+module resistor_cebedfe9 ();
+endmodule
+
+module resistor_75872dd5 ();
+endmodule
+
+module resistor_26a11865 ();
+endmodule
+
+// ------------------------ Module Verilog ---------------
+module resistor_ptype_accu (RN, RTN, RP2V, RP6V, RP12V, RP20V, RP24V, RP30V, RP36V, RP40V, RP45V, RP60V);
+  input  RN;
+  input  RTN;
+  input  RP2V;
+  input  RP6V;
+  input  RP12V;
+  input  RP20V;
+  input  RP24V;
+  input  RP30V;
+  input  RP36V;
+  input  RP40V;
+  input  RP45V;
+  input  RP60V;
+
+
+// ------------------------ Wires ------------------------
+
+// ------------------------ Networks ---------------------
+resistor_4fe26a31 XIXR2V (
+
+);
+
+resistor_1c79009d XIXR6V (
+
+);
+
+resistor_8659784c XIXR12V (
+
+);
+
+resistor_0eca3039 XIXR20V (
+
+);
+
+resistor_54fdf25c XIXR24V (
+
+);
+
+resistor_3f7b6fd7 XIXR30V (
+
+);
+
+resistor_982d13a7 XIXR36V (
+
+);
+
+resistor_cebedfe9 XIXR40V (
+
+);
+
+resistor_75872dd5 XIXR45V (
+
+);
+
+resistor_26a11865 XIXR60V (
+
+);
+
+endmodule
+
