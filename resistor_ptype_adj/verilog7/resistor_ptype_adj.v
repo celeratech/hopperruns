9386f2a27f601e4cc8839d5b246687b87af4a249
Hirshol Pheir <hirshol@gmail.com>
2024-10-14 00:16:19 +0000

    hopper run resistor_ptype_adj_20241014.00.16.19

---
+++ resistor_ptype_adj/verilog7/resistor_ptype_adj.v
@@ -0,0 +1,92 @@
+// ------------------------ Module Definitions -----------
+module resistor_cb1a970b ();
+endmodule
+
+module resistor_3a85703e ();
+endmodule
+
+module resistor_a4fccba1 ();
+endmodule
+
+module resistor_b3d2263f ();
+endmodule
+
+module resistor_726866a5 ();
+endmodule
+
+module resistor_abf59018 ();
+endmodule
+
+module resistor_f2b74118 ();
+endmodule
+
+module resistor_0023fff8 ();
+endmodule
+
+module resistor_7eda9488 ();
+endmodule
+
+module resistor_df3121fe ();
+endmodule
+
+// ------------------------ Module Verilog ---------------
+module resistor_ptype_adj (RN, RTN, RP2V, RP6V, RP12V, RP20V, RP24V, RP30V, RP36V, RP40V, RP45V, RP60V);
+  input  RN;
+  input  RTN;
+  input  RP2V;
+  input  RP6V;
+  input  RP12V;
+  input  RP20V;
+  input  RP24V;
+  input  RP30V;
+  input  RP36V;
+  input  RP40V;
+  input  RP45V;
+  input  RP60V;
+
+
+// ------------------------ Wires ------------------------
+
+// ------------------------ Networks ---------------------
+resistor_cb1a970b XIXR2V (
+
+);
+
+resistor_3a85703e XIXR6V (
+
+);
+
+resistor_a4fccba1 XIXR12V (
+
+);
+
+resistor_b3d2263f XIXR20V (
+
+);
+
+resistor_726866a5 XIXR24V (
+
+);
+
+resistor_abf59018 XIXR30V (
+
+);
+
+resistor_f2b74118 XIXR36V (
+
+);
+
+resistor_0023fff8 XIXR40V (
+
+);
+
+resistor_7eda9488 XIXR45V (
+
+);
+
+resistor_df3121fe XIXR60V (
+
+);
+
+endmodule
+
---
+++ resistor_ptype_adj/verilog8/root.v
@@ -0,0 +1,72 @@
+// ------------------------ Module Definitions -----------
+module FORCE_resistor_ptype_adj (RN,GND,RTN,RP2V,RP6V,RP12V,RP20V,RP24V,RP30V,RP36V,RP40V,RP45V,RP60V);
+  input  RN;
+  input  GND;
+  input  RTN;
+  input  RP2V;
+  input  RP6V;
+  input  RP12V;
+  input  RP20V;
+  input  RP24V;
+  input  RP30V;
+  input  RP36V;
+  input  RP40V;
+  input  RP45V;
+  input  RP60V;
+endmodule
+
+module resistor_ptype_adj (RN,RTN,RP2V,RP6V,RP12V,RP20V,RP24V,RP30V,RP36V,RP40V,RP45V,RP60V);
+  input  RN;
+  input  RTN;
+  input  RP2V;
+  input  RP6V;
+  input  RP12V;
+  input  RP20V;
+  input  RP24V;
+  input  RP30V;
+  input  RP36V;
+  input  RP40V;
+  input  RP45V;
+  input  RP60V;
+endmodule
+
+// ------------------------ Module Verilog ---------------
+module root ();
+
+
+// ------------------------ Wires ------------------------
+
+// ------------------------ Networks ---------------------
+FORCE_resistor_ptype_adj XFORCE_resistor_ptype_adj1 (
+.RN(net_28),
+.GND(net_0),
+.RTN(net_27),
+.RP2V(net_38),
+.RP6V(net_37),
+.RP12V(net_36),
+.RP20V(net_35),
+.RP24V(net_34),
+.RP30V(net_33),
+.RP36V(net_32),
+.RP40V(net_31),
+.RP45V(net_30),
+.RP60V(net_29)
+);
+
+resistor_ptype_adj Xresistor_ptype_adj1 (
+.RN(net_28),
+.RTN(net_27),
+.RP2V(net_38),
+.RP6V(net_37),
+.RP12V(net_36),
+.RP20V(net_35),
+.RP24V(net_34),
+.RP30V(net_33),
+.RP36V(net_32),
+.RP40V(net_31),
+.RP45V(net_30),
+.RP60V(net_29)
+);
+
+endmodule
+
