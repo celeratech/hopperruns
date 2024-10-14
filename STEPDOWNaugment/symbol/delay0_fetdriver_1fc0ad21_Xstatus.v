6b8b264729822e91e2b5b61eccc65f1f7e727cba
Hirshol Pheir <hirshol@gmail.com>
2024-10-14 17:08:29 +0000

    hopper run STEPDOWNaugment_20241014.17.08.29

---
+++ STEPDOWNaugment/symbol/delay0_fetdriver_1fc0ad21_Xstatus.v
@@ -0,0 +1,11 @@
+//Celera:delay0_fetdriver_1fc0ad21_Xstatus
+//TYPE: fixed 20ns
+module delay0_fetdriver_1fc0ad21_Xstatus (i, CELV, o,
+CELG,CELSUB);
+input CELV;
+input i;
+output o;
+input CELSUB;
+input CELG;
+endmodule
+
