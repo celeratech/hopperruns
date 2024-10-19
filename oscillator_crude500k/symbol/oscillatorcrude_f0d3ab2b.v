cccc5a868bffeadecf9c649477273b00e1859b59
Hirshol Pheir <hirshol@gmail.com>
2024-10-19 00:56:34 +0000

    hopper run oscillator_crude500k_20241019.00.56.34

---
+++ oscillator_crude500k/symbol/oscillatorcrude_f0d3ab2b.v
@@ -0,0 +1,18 @@
+//Celera:oscillatorcrude_f0d3ab2b
+//Celera Confidential Symbol Generator
+//VMAX:globalV,Crude:500.000KHz
+module oscillatorcrude_f0d3ab2b (SIMPV,ok_oscillator,osc,global_oscillator,
+enable_oscillator,
+IP,
+CELG,SENSE_G,CELSUB);
+input SIMPV;
+output ok_oscillator;
+output osc;
+input global_oscillator;
+input IP;
+input enable_oscillator;
+input CELG;
+input SENSE_G;
+input CELSUB;
+endmodule
+
