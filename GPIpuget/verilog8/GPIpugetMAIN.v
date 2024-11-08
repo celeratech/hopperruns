2b8b74e61cbd53e4a5f2674f2f143c251b4ce3de
Hirshol Pheir <hirshol@gmail.com>
2024-11-08 18:28:57 +0000

    hopper run GPIpuget_20241108.18.28.57

--- GPIpuget/verilog8/GPIpugetMAIN.v
+++ GPIpuget/verilog8/GPIpugetMAIN.v
@@ -258,8 +258,8 @@ vbuffer_749dfb87 XU6 (
 .ok_vbuffer(net_111),
 .enable_vbuffer(net_110),
 .global_vbuffer(tl0),
-.trim_vbuffer_negative({a0,a0,a0,a0,a0,a0,a0}),
-.trim_vbuffer_positive({a0,a0,a0,a0,a0,a0,a0})
+.trim_vbuffer_negative(a0[6:0]),
+.trim_vbuffer_positive(a0[6:0])
 );
 
 switchgnd_d6772c2d XU8 (
