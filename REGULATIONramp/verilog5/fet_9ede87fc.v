a94ffafe9ed5b178fc5ac6023cf6fd55e4694546
Hirshol Pheir <hirshol@gmail.com>
2024-11-14 14:35:33 +0000

    hopper run REGULATIONramp_20241114.14.35.33

--- REGULATIONramp/verilog5/fet_9ede87fc.v
+++ REGULATIONramp/verilog5/fet_9ede87fc.v
@@ -9,6 +9,15 @@
 //BODY DIODE:yes
 //KEVLIN:no
 
+//Celera Confidential Do Not Copy NMOS
+module pch5i2_5t_3p7x0p6x3p0x1p0 (DRAIN,GATE,SOURCE,BULK,SUB);
+  input GATE;
+  input BULK;
+  input SUB;
+  inout SOURCE;
+  inout DRAIN;
+endmodule
+
 //Celera Confidential Do Not Copy fet_9ede87fc
 //Celera Confidential Symbol Generator
 //signal PMOS:Ron:1000 Ohm
@@ -23,6 +32,16 @@ inout SOURCE;
 inout DRAIN;
 input SUB;
 input PMOSiso6;
+
+//Celera Confidential Do Not Copy pch5i2_5t_3p7x0p6x3p0x1p0
+pch5i2_5t_3p7x0p6x3p0x1p0 Xpmos0(
+.DRAIN (DRAIN),
+.GATE (GATE),
+.SOURCE (SOURCE),
+.BULK (PMOSiso6),
+.SUB (SUB)
+);
+//,diesize,pch5i2_5t_3p7x0p6x3p0x1p0
 
 //Celera Confidential Do Not Copy Module End
 //Celera Schematic Generator
