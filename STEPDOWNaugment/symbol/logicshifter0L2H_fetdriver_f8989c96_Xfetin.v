b886fbe6138913400ec1d3076a41c465876f442c
Hirshol Pheir <hirshol@gmail.com>
2024-11-14 15:29:47 +0000

    hopper run STEPDOWNaugment_20241114.15.29.47

---
+++ STEPDOWNaugment/symbol/logicshifter0L2H_fetdriver_f8989c96_Xfetin.v
@@ -0,0 +1,16 @@
+//Celera:logicshifter0L2H_fetdriver_f8989c96_Xfetin
+//Logic Level shifter with Enable
+module logicshifter0L2H_fetdriver_f8989c96_Xfetin (enable_logicshifter,
+VIN1,VIN2,
+in,
+out,
+CELG,CELSUB);
+input VIN1;
+input VIN2;
+input in;
+output out;
+input enable_logicshifter;
+input CELSUB;
+input CELG;
+endmodule
+
