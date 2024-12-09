1808c0ecd14d6e7378e0ea024f302f0f56f12db7
Hirshol Pheir <hirshol@gmail.com>
2024-12-09 21:58:37 +0000

    hopper run STEPDOWNaugment_20241209.21.58.36

---
+++ STEPDOWNaugment/symbol/fet_fet_5e81270d_Xfet.v
@@ -0,0 +1,13 @@
+//Celera:fet_fet_5e81270d_Xfet
+//Celera Confidential Symbol Generator
+//power NMOS:Ron:0.100 Ohm
+//Vgs 6V Vds 30V
+//Kelvin:no
+
+module fet_fet_5e81270d_Xfet (GATE,SOURCE,DRAIN,SUB);
+input GATE;
+inout SOURCE;
+inout DRAIN;
+input SUB;
+endmodule
+
