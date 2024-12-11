641f3afee4d0e94a1a36cc0510b964470d67cf1f
Hirshol Pheir <hirshol@gmail.com>
2024-12-11 16:35:16 +0000

    hopper run STEPDOWNaugment_20241211.16.35.16

---
+++ STEPDOWNaugment/symbol/currentmirror1_bad7dda9.v
@@ -0,0 +1,18 @@
+//Celera:currentmirror1_bad7dda9
+//Celera Confidential Symbol Generator
+//Polarity: source, Maximum Current: 20, Number of outputs: 1, DFT: no, Max Vout: 6
+//GAIN0:5, TYPE0:source
+module currentmirror1_bad7dda9 (SIMPV,CELSUB,enable_currentmirror,ISET,ok_currentmirror,
+I0,
+global_currentmirror,
+CELG);
+input SIMPV;
+input CELG;
+input CELSUB;
+input enable_currentmirror;
+input ISET;
+output ok_currentmirror;
+inout I0;
+input global_currentmirror;
+endmodule
+
