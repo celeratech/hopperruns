89c93b0b749aefd3fac6f96dca4255f6b652fe85
Hirshol Pheir <hirshol@gmail.com>
2024-12-12 02:33:45 +0000

    hopper run STEPDOWNaugment_20241212.02.33.45

---
+++ STEPDOWNaugment/symbol/fet_c86247ba.v
@@ -0,0 +1,14 @@
+//Celera:fet_c86247ba
+//Celera Confidential Symbol Generator
+//power NMOS:Ron:0.100 Ohm
+//Vgs 6V Vds 6V
+//Kelvin:no
+
+module fet_c86247ba (GATE,SOURCE,DRAIN,NMOSiso6,SUB);
+input GATE;
+inout SOURCE;
+inout DRAIN;
+input SUB;
+input NMOSiso6;
+endmodule
+
