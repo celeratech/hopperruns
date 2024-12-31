fd37aaf341d8e247c3af3f339944b97ac8d8466d
Hirshol Pheir <hirshol@gmail.com>
2024-12-31 20:59:22 +0000

    hopper run TELEMETRYadcCLOCK_20241231.20.59.21

---
+++ TELEMETRYadcCLOCK/symbol/currentmirror_369e5658.v
@@ -0,0 +1,16 @@
+//Celera:currentmirror_369e5658
+//Celera Confidential Symbol Generator
+//Polarity: source, Maximum Current: 10, Number of outputs: 1, DFT: no, Max Vout: 6
+//GAIN0:1, TYPE0:source
+module currentmirror_369e5658 (CELV,CELSUB,enable_currentmirror,ISET,ok_currentmirror,
+I0,
+CELG);
+input CELV;
+input CELG;
+input CELSUB;
+input enable_currentmirror;
+input ISET;
+output ok_currentmirror;
+inout I0;
+endmodule
+
