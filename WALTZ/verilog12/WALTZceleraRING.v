c4449cd86d2c6f1d56084c501436f8cd0d44c447
Hirshol Pheir <hirshol@gmail.com>
2025-02-05 03:36:05 +0000

    hopper run WALTZ_20250205.03.36.04

--- WALTZ/verilog12/WALTZceleraRING.v
+++ WALTZ/verilog12/WALTZceleraRING.v
@@ -121,7 +121,7 @@ inout  GND;
 inout  PIN;
 inout  POK;
 inout  VCC;
-inout [5:0] tmi;
+inout [0:4] tmi;
 inout  BIAS;
 inout  FSET;
 output  GOTP;
@@ -152,7 +152,7 @@ output  celkelvin_BIAS_bc3b7675;
 
 
 // ------------------------ Wires ------------------------
-wire [5:0] tmi;
+wire [4:0] tmi;
 wire [7:0] revision;
 wire [7:0] chipidlsb;
 wire [7:0] chipidmsb;
--- WALTZ/verilog13/WALTZ.v
+++ WALTZ/verilog13/WALTZ.v
@@ -45,7 +45,7 @@ module WALTZceleraRING (EN,FB,IN,SW,BST,GND,PIN,POK,VCC,tmi,BIAS,FSET,GOTP,PGND,
   inout  PIN;
   inout  POK;
   inout  VCC;
-  inout [5:0] tmi;
+  inout [0:4] tmi;
   inout  BIAS;
   inout  FSET;
   output  GOTP;
@@ -167,7 +167,7 @@ WALTZceleraRING XceleraRING (
 .PIN(PIN),
 .POK(POK),
 .VCC(VCC),
-.tmi(>[5:0]),
+.tmi(tmi[0:4]),
 .BIAS(BIAS),
 .FSET(FSET),
 .GOTP(GOTP),
