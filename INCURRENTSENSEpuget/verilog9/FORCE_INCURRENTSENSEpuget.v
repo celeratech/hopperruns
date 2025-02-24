aa18b099a1ea49382e310e6542fac1298883a8f5
Hirshol Pheir <hirshol@gmail.com>
2025-02-24 19:53:18 +0000

    hopper run INCURRENTSENSEpuget_20250224.19.53.18

--- INCURRENTSENSEpuget/verilog9/FORCE_INCURRENTSENSEpuget.v
+++ INCURRENTSENSEpuget/verilog9/FORCE_INCURRENTSENSEpuget.v
@@ -63,7 +63,12 @@ module vpwl_0x0_10d0ux0_11d0ux5 (PLUS,MINUS);
   output  MINUS;
 endmodule
 
-module vpwl_0x0_10d0ux0_11d0ux0d032 (PLUS,MINUS);
+module vpwl_0x0_10d0ux0_11d0ux11d968 (PLUS,MINUS);
+  output  PLUS;
+  output  MINUS;
+endmodule
+
+module vpwl_0x0_10d0ux0_11d0ux12 (PLUS,MINUS);
   output  PLUS;
   output  MINUS;
 endmodule
@@ -125,7 +130,7 @@ STONEnoconn XNCip (
 );
 
 dbuf_e926e395 Xd_clock_currentsensein (
-.i(net_136),
+.i(net_148),
 .o(clock_currentsensein),
 .SUB(CELSUB40948),
 .CELG(CELG59462),
@@ -143,7 +148,7 @@ WRAPPER1 XWRAPregister_measure_currentsensein_xd_measure_currentsensein_824d3563
 );
 
 vpulse_0x1x1d0ux0x1nx1nx500d0n XV2 (
-.PLUS(net_136),
+.PLUS(net_148),
 .MINUS(GND)
 );
 
@@ -187,12 +192,12 @@ vdc_0 VI__SIMPV (
 .MINUS(SIMPV)
 );
 
-vpwl_0x0_10d0ux0_11d0ux0d032 V_VOUTSNx (
+vpwl_0x0_10d0ux0_11d0ux11d968 V_VOUTSNx (
 .PLUS(net_59),
 .MINUS(GND)
 );
 
-vpwl_0x0_10d0ux0_11d0ux0d032 V_VOUTSPx (
+vpwl_0x0_10d0ux0_11d0ux12 V_VOUTSPx (
 .PLUS(net_58),
 .MINUS(GND)
 );
