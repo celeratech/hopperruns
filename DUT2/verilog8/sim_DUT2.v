bb2aff17dd7e6201ddf3aeb9219ea427b5a7ce8e
Hirshol Pheir <hirshol@gmail.com>
2025-02-25 02:35:36 +0000

    hopper run DUT2_20250225.02.35.36

--- DUT2/verilog8/sim_DUT2.v
+++ DUT2/verilog8/sim_DUT2.v
@@ -1,5 +1,5 @@
 // ------------------------ Module Definitions -----------
-module DUT2 (BS_0,BS_1,BS_2,BS_3,ISIN,ITRI,VPWL,VSQR,VTRI,BSO_0,BSO_1,BSO_2,BSO_3,COMPOUT,CELG59462,CELV96848,PORB97836,CELSUB40948,SENSE_G_465a3572);
+module DUT2 (BS_0,BS_1,BS_2,BS_3,ISIN,ITRI,VPWL,VSQR,VTRI,BSO_0,BSO_1,BSO_2,BSO_3,COMPOUT,CELG59462,CELV96848,PORB97836,CELSUB40948,IP_efc25439,SENSE_G_465a3572);
   input  BS_0;
   input  BS_1;
   input  BS_2;
@@ -18,48 +18,43 @@ module DUT2 (BS_0,BS_1,BS_2,BS_3,ISIN,ITRI,VPWL,VSQR,VTRI,BSO_0,BSO_1,BSO_2,BSO_
   input  CELV96848;
   input  PORB97836;
   input  CELSUB40948;
+  input  IP_efc25439;
   input  SENSE_G_465a3572;
 endmodule
 
 module EXTERNALDUT2 (GND,IPUL,IPWL,ISIN,ISQR,ITRI,VPUL,VSIN,VTRI,BSO_0,BSO_1,BSO_2,BSO_3,COMPOUT);
-  inout  GND;
-  inout  IPUL;
-  inout  IPWL;
-  inout  ISIN;
-  inout  ISQR;
-  inout  ITRI;
-  inout  VPUL;
-  inout  VSIN;
+  input  GND;
+  input  IPUL;
+  input  IPWL;
+  input  ISIN;
+  input  ISQR;
+  input  ITRI;
+  input  VPUL;
+  input  VSIN;
   input  VTRI;
   input  BSO_0;
   input  BSO_1;
   input  BSO_2;
   input  BSO_3;
-  inout  COMPOUT;
+  input  COMPOUT;
 endmodule
 
-module FORCEDUT2 (GND,BS_0,BS_1,BS_2,BS_3,IPUL,IPWL,ISIN,ISQR,ITRI,VPUL,VPWL,VSIN,VSQR,VTRI,CELG59462,CELV96848,PORB97836,CELSUB40948,IP_efc25439,SENSE_G_465a3572);
-  inout  GND;
-  inout  BS_0;
-  inout  BS_1;
-  inout  BS_2;
-  inout  BS_3;
-  inout  IPUL;
-  output  IPWL;
-  inout  ISIN;
-  inout  ISQR;
-  inout  ITRI;
-  inout  VPUL;
-  output  VPWL;
-  inout  VSIN;
+module FORCEDUT2 (GND,BS_0,BS_1,BS_2,BS_3,IPUL,IPWL,ISIN,ISQR,ITRI,VPUL,VPWL,VSIN,VSQR,VTRI);
+  input  GND;
+  input  BS_0;
+  input  BS_1;
+  input  BS_2;
+  input  BS_3;
+  input  IPUL;
+  input  IPWL;
+  input  ISIN;
+  input  ISQR;
+  input  ITRI;
+  input  VPUL;
+  input  VPWL;
+  input  VSIN;
   output  VSQR;
-  inout  VTRI;
-  output  CELG59462;
-  output  CELV96848;
-  output  PORB97836;
-  output  CELSUB40948;
-  output  IP_efc25439;
-  output  SENSE_G_465a3572;
+  input  VTRI;
 endmodule
 
 // ------------------------ Module Verilog ---------------
@@ -88,6 +83,7 @@ DUT2 XDUT2 (
 .CELV96848(CELV96848),
 .PORB97836(PORB97836),
 .CELSUB40948(CELSUB40948),
+.IP_efc25439(IP_efc25439),
 .SENSE_G_465a3572(SENSE_G_465a3572)
 );
 
@@ -123,13 +119,7 @@ FORCEDUT2 XFORCEDUT2 (
 .VPWL(VPWL),
 .VSIN(VSIN),
 .VSQR(VSQR),
-.VTRI(VTRI),
-.CELG59462(CELG59462),
-.CELV96848(CELV96848),
-.PORB97836(PORB97836),
-.CELSUB40948(CELSUB40948),
-.IP_efc25439(IP_efc25439),
-.SENSE_G_465a3572(SENSE_G_465a3572)
+.VTRI(VTRI)
 );
 
 endmodule
