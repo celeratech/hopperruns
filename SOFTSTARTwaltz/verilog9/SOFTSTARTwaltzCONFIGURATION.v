9d8ce22ee8f7ebe8e919b5a058d6274e77ab5806
Hirshol Pheir <hirshol@gmail.com>
2025-03-12 20:02:02 +0000

    hopper run SOFTSTARTwaltz_20250312.20.02.01

--- SOFTSTARTwaltz/verilog9/SOFTSTARTwaltzCONFIGURATION.v
+++ SOFTSTARTwaltz/verilog9/SOFTSTARTwaltzCONFIGURATION.v
@@ -1,28 +1,30 @@
 // ------------------------ Module Definitions -----------
-//Verilog HDL for "Generate", "STONEnoconn" "functional"
+//Verilog HDL for "PEBBLES", "PEBBLEtielo" "functional"
 
 
-module STONEnoconn ( noconn );
+module PEBBLEtielo ( q, G, SUB, V );
 
-  input noconn;
+  input V;
+  output q;
+  input G;
+  input SUB;
 endmodule
 
 
 // ------------------------ Module Verilog ---------------
 module SOFTSTARTwaltzCONFIGURATION (halfway, softstart_1ms);
-input  halfway;
-input  softstart_1ms;
+output  halfway;
+output  softstart_1ms;
 
 
 // ------------------------ Wires ------------------------
 
 // ------------------------ Networks ---------------------
-STONEnoconn XNChalfway (
-.noconn(halfway)
-);
-
-STONEnoconn XNCsoftstart_1ms (
-.noconn(softstart_1ms)
+PEBBLEtielo XDRMNOTL (
+.G(CELG59462),
+.V(CELV96848),
+.q(a0),
+.SUB(CELSUB40948)
 );
 
 endmodule
