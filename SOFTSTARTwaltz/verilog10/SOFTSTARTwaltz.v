9c72228a1a059b003021bf41c3ecc1e3f5d9c838
Hirshol Pheir <hirshol@gmail.com>
2025-03-18 15:55:14 +0000

    hopper run SOFTSTARTwaltz_20250318.15.55.13

--- SOFTSTARTwaltz/verilog10/SOFTSTARTwaltz.v
+++ SOFTSTARTwaltz/verilog10/SOFTSTARTwaltz.v
@@ -1,10 +1,10 @@
 // ------------------------ Module Definitions -----------
 module SOFTSTARTwaltzCONFIGURATION (halfway,CELG59462,CELV96848,CELSUB40948,softstart_1ms);
-  output  halfway;
+  input  halfway;
   input  CELG59462;
   input  CELV96848;
   input  CELSUB40948;
-  output  softstart_1ms;
+  input  softstart_1ms;
 endmodule
 
 module SOFTSTARTwaltzDEBUG (SS,CELG59462,CELV96848,dft_clock,CELSUB40948,enable_brick,done_softstart,enable_softstart,hijack_enable_brick,hijack_enable_softstart);
--- SOFTSTARTwaltz/verilog9/SOFTSTARTwaltzCONFIGURATION.v
+++ SOFTSTARTwaltz/verilog9/SOFTSTARTwaltzCONFIGURATION.v
@@ -1,4 +1,24 @@
 // ------------------------ Module Definitions -----------
+//Verilog HDL for "Generate", "STONEnoconn" "functional"
+
+
+module STONEnoconn ( noconn );
+
+  input noconn;
+endmodule
+
+
+//Verilog HDL for "Generate", "WRAPPER2" "functional"
+
+
+module WRAPPER2 ( i, o0, o1 );
+
+  inout o1;
+  inout o0;
+  inout  [1:0] i;
+endmodule
+
+
 //Verilog HDL for "PEBBLES", "PEBBLEtielo" "functional"
 
 
@@ -13,27 +33,30 @@ endmodule
 
 // ------------------------ Module Verilog ---------------
 module SOFTSTARTwaltzCONFIGURATION (halfway, CELG59462, CELV96848, CELSUB40948, softstart_1ms);
-output  halfway;
+  input  halfway;
 input  CELG59462;
 input  CELV96848;
 input  CELSUB40948;
-output  softstart_1ms;
+  input  softstart_1ms;
 
 
 // ------------------------ Wires ------------------------
 
 // ------------------------ Networks ---------------------
-PEBBLEtielo XDRMNOTL_halfway (
-.G(CELG59462),
-.V(CELV96848),
-.q(halfway),
-.SUB(CELSUB40948)
+STONEnoconn XNCa0 (
+.noconn(a0)
 );
 
-PEBBLEtielo XDRMNOTL_softstart_1ms (
+WRAPPER2 WRAPPER2 (
+.i(net_0),
+.o0(softstart_1ms),
+.o1(halfway)
+);
+
+PEBBLEtielo XDRMNOTL (
 .G(CELG59462),
 .V(CELV96848),
-.q(softstart_1ms),
+.q(a0),
 .SUB(CELSUB40948)
 );
 
