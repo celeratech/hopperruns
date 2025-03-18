a4f0f8e7ba46c114c7a1c01372c80290a2bce440
Hirshol Pheir <hirshol@gmail.com>
2025-03-18 21:05:25 +0000

    hopper run SOFTSTARTwaltz_20250318.21.05.25

--- SOFTSTARTwaltz/verilog9/SOFTSTARTwaltzCONFIGURATION.v
+++ SOFTSTARTwaltz/verilog9/SOFTSTARTwaltzCONFIGURATION.v
@@ -1,16 +1,4 @@
 // ------------------------ Module Definitions -----------
-//Verilog HDL for "PEBBLES", "PEBBLEtiehi" "functional"
-
-
-module PEBBLEtiehi ( q, G, SUB, V );
-
-  input V;
-  output q;
-  input G;
-  input SUB;
-endmodule
-
-
 //Verilog HDL for "PEBBLES", "PEBBLEtielo" "functional"
 
 
@@ -45,13 +33,6 @@ output  softstart_1ms;
 // ------------------------ Wires ------------------------
 
 // ------------------------ Networks ---------------------
-PEBBLEtiehi XDRMNOTH (
-.G(CELG59462),
-.V(CELV96848),
-.q(a1),
-.SUB(CELSUB40948)
-);
-
 PEBBLEtielo XDRMNOTL (
 .G(CELG59462),
 .V(CELV96848),
@@ -60,7 +41,7 @@ PEBBLEtielo XDRMNOTL (
 );
 
 WRAPPER1 halfway_WRAPPER (
-.i(a1),
+.i(a0),
 .o(halfway)
 );
 
