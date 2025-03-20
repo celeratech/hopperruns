d864c7204a571484f5498cf183361f9e88274f2c
Hirshol Pheir <hirshol@gmail.com>
2025-03-20 23:06:36 +0000

    hopper run REGULATIONwaltz_20250320.23.06.36

--- REGULATIONwaltz/verilog10/sim_REGULATIONwaltz_ROW26.v
+++ REGULATIONwaltz/verilog10/sim_REGULATIONwaltz_ROW26.v
@@ -1,14 +1,14 @@
 // ------------------------ Module Definitions -----------
-module FORCE_REGULATIONwaltz (GND,MUDG,MUDV,CELG59462,CELV96848,CELSUB40948,IP_70133221,IP_96171c99,IP_df440ad3,IP_f5d94fdd,IP_fa70de6e,kelvin_MUDG,FB_REGULATION,REF_REGULATION,VSS_REGULATION,enable_regulation);
+module FORCE_REGULATIONwaltz (GND,MUDG,MUDV,CELG59462,CELV96848,CELSUB40948,IP_0d0816e7,IP_70133221,IP_96171c99,IP_f5d94fdd,IP_fa70de6e,kelvin_MUDG,FB_REGULATION,REF_REGULATION,VSS_REGULATION,enable_regulation);
   output  GND;
   output  MUDG;
   output  MUDV;
   output  CELG59462;
   output  CELV96848;
   output  CELSUB40948;
+  output  IP_0d0816e7;
   output  IP_70133221;
   output  IP_96171c99;
-  output  IP_df440ad3;
   output  IP_f5d94fdd;
   output  IP_fa70de6e;
   output  kelvin_MUDG;
@@ -18,7 +18,7 @@ module FORCE_REGULATIONwaltz (GND,MUDG,MUDV,CELG59462,CELV96848,CELSUB40948,IP_7
   inout  enable_regulation;
 endmodule
 
-module REGULATIONwaltz (tmi,MUDG,MUDV,CELG59462,CELV96848,go_driver,CELSUB40948,IP_70133221,IP_96171c99,IP_df440ad3,IP_f5d94fdd,IP_fa70de6e,IREF_DRIVER,kelvin_MUDG,FB_REGULATION,ok_regulation,REF_REGULATION,VSS_REGULATION,enable_regulation);
+module REGULATIONwaltz (tmi,MUDG,MUDV,CELG59462,CELV96848,go_driver,CELSUB40948,IP_0d0816e7,IP_70133221,IP_96171c99,IP_f5d94fdd,IP_fa70de6e,IREF_DRIVER,kelvin_MUDG,FB_REGULATION,ok_regulation,REF_REGULATION,VSS_REGULATION,enable_regulation);
   inout [4:0] tmi;
   inout  MUDG;
   input  MUDV;
@@ -26,9 +26,9 @@ module REGULATIONwaltz (tmi,MUDG,MUDV,CELG59462,CELV96848,go_driver,CELSUB40948,
   input  CELV96848;
   output  go_driver;
   input  CELSUB40948;
+  input  IP_0d0816e7;
   input  IP_70133221;
   input  IP_96171c99;
-  input  IP_df440ad3;
   input  IP_f5d94fdd;
   input  IP_fa70de6e;
   inout  IREF_DRIVER;
@@ -41,7 +41,7 @@ module REGULATIONwaltz (tmi,MUDG,MUDV,CELG59462,CELV96848,go_driver,CELSUB40948,
 endmodule
 
 // ------------------------ Module Verilog ---------------
-module sim_REGULATIONwaltz_ROW17 ();
+module sim_REGULATIONwaltz_ROW26 ();
 
 
 // ------------------------ Wires ------------------------
@@ -50,14 +50,14 @@ wire [4:0] tmi;
 // ------------------------ Networks ---------------------
 FORCE_REGULATIONwaltz XFORCE_REGULATIONwaltz1 (
 .GND(GND),
-.MUDG(MUDG),
+.MUDG(kelvin_MUDG),
 .MUDV(MUDV),
 .CELG59462(CELG59462),
 .CELV96848(CELV96848),
 .CELSUB40948(CELSUB40948),
+.IP_0d0816e7(IP_0d0816e7),
 .IP_70133221(IP_70133221),
 .IP_96171c99(IP_96171c99),
-.IP_df440ad3(IP_df440ad3),
 .IP_f5d94fdd(IP_f5d94fdd),
 .IP_fa70de6e(IP_fa70de6e),
 .kelvin_MUDG(kelvin_MUDG),
@@ -69,15 +69,15 @@ FORCE_REGULATIONwaltz XFORCE_REGULATIONwaltz1 (
 
 REGULATIONwaltz XREGULATIONwaltz1 (
 .tmi(tmi[4:0]),
-.MUDG(MUDG),
+.MUDG(kelvin_MUDG),
 .MUDV(MUDV),
 .CELG59462(CELG59462),
 .CELV96848(CELV96848),
 .go_driver(go_driver),
 .CELSUB40948(CELSUB40948),
+.IP_0d0816e7(IP_0d0816e7),
 .IP_70133221(IP_70133221),
 .IP_96171c99(IP_96171c99),
-.IP_df440ad3(IP_df440ad3),
 .IP_f5d94fdd(IP_f5d94fdd),
 .IP_fa70de6e(IP_fa70de6e),
 .IREF_DRIVER(IREF_DRIVER),
--- REGULATIONwaltz/verilog9/REGULATIONwaltz.v
+++ REGULATIONwaltz/verilog9/REGULATIONwaltz.v
@@ -1,24 +1,4 @@
 // ------------------------ Module Definitions -----------
-module REGULATIONwaltzCONFIGURATION (tmi,CZCOMP_0,CZCOMP_1,CZCOMP_2,RZCOMP_0,RZCOMP_1,RZCOMP_2,CELG59462,CELV96848,GAINCOMP_0,GAINCOMP_1,vss_status,CELSUB40948,DCMRIPPLE_0,DCMRIPPLE_1,vc_testmode,vss_testmode);
-  inout [4:0] tmi;
-  input  CZCOMP_0;
-  input  CZCOMP_1;
-  input  CZCOMP_2;
-  input  RZCOMP_0;
-  input  RZCOMP_1;
-  input  RZCOMP_2;
-  input  CELG59462;
-  input  CELV96848;
-  input  GAINCOMP_0;
-  input  GAINCOMP_1;
-  input  vss_status;
-  input  CELSUB40948;
-  input  DCMRIPPLE_0;
-  input  DCMRIPPLE_1;
-  input  vc_testmode;
-  input  vss_testmode;
-endmodule
-
 module REGULATIONwaltzDEBUG (VC,REFINT,CELG59462,CELV96848,go_driver,vss_status,CELSUB40948,IREF_DRIVER,vc_testmode,vss_testmode,FB_REGULATION,ok_regulation,VSS_REGULATION,enable_regulation,HIJACK_VSS_REGULATION,hijack_enable_regulation);
   inout  VC;
   input  REFINT;
@@ -38,9 +18,8 @@ module REGULATIONwaltzDEBUG (VC,REFINT,CELG59462,CELV96848,go_driver,vss_status,
   output  hijack_enable_regulation;
 endmodule
 
-module REGULATIONwaltzMAIN (VC,tmi,MUDG,MUDV,REFINT,CZCOMP_0,CZCOMP_1,CZCOMP_2,RZCOMP_0,RZCOMP_1,RZCOMP_2,CELG59462,CELV96848,go_driver,GAINCOMP_0,GAINCOMP_1,CELSUB40948,DCMRIPPLE_0,DCMRIPPLE_1,IP_70133221,IP_96171c99,IP_df440ad3,IP_f5d94fdd,IP_fa70de6e,IREF_DRIVER,kelvin_MUDG,FB_REGULATION,ok_regulation,REF_REGULATION,VSS_REGULATION,CELREF_e3b0c442,enable_regulation);
+module REGULATIONwaltzMAIN (VC,MUDG,MUDV,REFINT,CZCOMP_0,CZCOMP_1,CZCOMP_2,RZCOMP_0,RZCOMP_1,RZCOMP_2,CELG59462,CELV96848,go_driver,GAINCOMP_0,GAINCOMP_1,CELSUB40948,DCMRIPPLE_0,DCMRIPPLE_1,IP_0d0816e7,IP_70133221,IP_96171c99,IP_f5d94fdd,IP_fa70de6e,IREF_DRIVER,kelvin_MUDG,FB_REGULATION,ok_regulation,REF_REGULATION,VSS_REGULATION,CELREF_e3b0c442,enable_regulation,trim_clamp_0d0816e7,trim_gm_negative_70133221,trim_gm_positive_70133221,trim_vbuffer_negative_96171c99,trim_vbuffer_positive_96171c99);
   inout  VC;
-  inout [4:0] tmi;
   inout  MUDG;
   input  MUDV;
   inout  REFINT;
@@ -58,9 +37,9 @@ module REGULATIONwaltzMAIN (VC,tmi,MUDG,MUDV,REFINT,CZCOMP_0,CZCOMP_1,CZCOMP_2,R
   input  CELSUB40948;
   input  DCMRIPPLE_0;
   input  DCMRIPPLE_1;
+  input  IP_0d0816e7;
   input  IP_70133221;
   input  IP_96171c99;
-  input  IP_df440ad3;
   input  IP_f5d94fdd;
   input  IP_fa70de6e;
   inout  IREF_DRIVER;
@@ -71,10 +50,56 @@ module REGULATIONwaltzMAIN (VC,tmi,MUDG,MUDV,REFINT,CZCOMP_0,CZCOMP_1,CZCOMP_2,R
   input  VSS_REGULATION;
   input  CELREF_e3b0c442;
   input  enable_regulation;
+  input [7:0] trim_clamp_0d0816e7;
+  input [6:0] trim_gm_negative_70133221;
+  input [6:0] trim_gm_positive_70133221;
+  input [6:0] trim_vbuffer_negative_96171c99;
+  input [6:0] trim_vbuffer_positive_96171c99;
 endmodule
 
+//Verilog HDL for "DRM", "drm56" "functional"
+
+
+module drm56 ( V, G, SUB, tmi, bypload, lastdrm, id, por0, por1, por2, por3,
+por4, por5, por6, drm0, drm1, drm2, drm3, drm4, drm5, drm6, d1, d0 );
+
+  output  [7:0] drm4;
+  input  [7:0] por6;
+  input lastdrm;
+  input V;
+  output d1;
+  input  [7:0] por3;
+  output  [7:0] drm3;
+  input  [7:0] por5;
+  input  [7:0] id;
+  output d0;
+  output  [7:0] drm5;
+  output  [7:0] drm2;
+  input  [7:0] por2;
+  input  [7:0] por1;
+  input  [7:0] por4;
+  input bypload;
+  output  [7:0] drm6;
+  output  [7:0] drm0;
+  input  [7:0] por0;
+  input G;
+  output  [7:0] drm1;
+  inout  [4:0] tmi;
+  input SUB;
+endmodule
+
+
+//Verilog HDL for "Generate", "STONEnoconn" "functional"
+
+
+module STONEnoconn ( noconn );
+
+  input noconn;
+endmodule
+
+
 // ------------------------ Module Verilog ---------------
-module REGULATIONwaltz (tmi, MUDG, MUDV, CELG59462, CELV96848, go_driver, CELSUB40948, IP_70133221, IP_96171c99, IP_df440ad3, IP_f5d94fdd, IP_fa70de6e, IREF_DRIVER, kelvin_MUDG, FB_REGULATION, ok_regulation, REF_REGULATION, VSS_REGULATION, enable_regulation);
+module REGULATIONwaltz (tmi, MUDG, MUDV, CELG59462, CELV96848, go_driver, CELSUB40948, IP_0d0816e7, IP_70133221, IP_96171c99, IP_f5d94fdd, IP_fa70de6e, IREF_DRIVER, kelvin_MUDG, FB_REGULATION, ok_regulation, REF_REGULATION, VSS_REGULATION, enable_regulation);
 inout [4:0] tmi;
 inout  MUDG;
 input  MUDV;
@@ -82,9 +107,9 @@ input  CELG59462;
 input  CELV96848;
 output  go_driver;
 input  CELSUB40948;
+input  IP_0d0816e7;
 input  IP_70133221;
 input  IP_96171c99;
-input  IP_df440ad3;
 input  IP_f5d94fdd;
 input  IP_fa70de6e;
 inout  IREF_DRIVER;
@@ -98,70 +123,69 @@ input  enable_regulation;
 
 // ------------------------ Wires ------------------------
 wire [4:0] tmi;
+wire [7:0] trim_clamp_0d0816e7;
+wire [6:0] trim_gm_negative_70133221;
+wire [6:0] trim_gm_positive_70133221;
+wire [6:0] trim_vbuffer_negative_96171c99;
+wire [6:0] trim_vbuffer_positive_96171c99;
+wire [7:0] id;
+wire [7:0] drm0;
+wire [7:0] drm1;
+wire [7:0] drm2;
+wire [7:0] drm3;
+wire [7:0] drm4;
+wire [7:0] drm5;
+wire [7:0] drm6;
+wire [7:0] por0;
+wire [7:0] por1;
+wire [7:0] por2;
+wire [7:0] por3;
+wire [7:0] por4;
+wire [7:0] por5;
+wire [7:0] por6;
 
 // ------------------------ Networks ---------------------
-REGULATIONwaltzCONFIGURATION XCONFIGURATION (
-.tmi(tmi[4:0]),
-.CZCOMP_0(net_52),
-.CZCOMP_1(net_53),
-.CZCOMP_2(net_54),
-.RZCOMP_0(net_55),
-.RZCOMP_1(net_56),
-.RZCOMP_2(net_57),
-.CELG59462(CELG59462),
-.CELV96848(CELV96848),
-.GAINCOMP_0(net_58),
-.GAINCOMP_1(net_59),
-.vss_status(net_72),
-.CELSUB40948(CELSUB40948),
-.DCMRIPPLE_0(net_50),
-.DCMRIPPLE_1(net_51),
-.vc_testmode(net_70),
-.vss_testmode(net_71)
-);
-
 REGULATIONwaltzDEBUG XDEBUG (
-.VC(net_75),
-.REFINT(net_73),
+.VC(net_142),
+.REFINT(net_140),
 .CELG59462(CELG59462),
 .CELV96848(CELV96848),
 .go_driver(go_driver),
-.vss_status(net_72),
+.vss_status(REGULATIONtestmode_5fb9e860_2),
 .CELSUB40948(CELSUB40948),
 .IREF_DRIVER(IREF_DRIVER),
-.vc_testmode(net_70),
-.vss_testmode(net_71),
+.vc_testmode(REGULATIONtestmode_5fb9e860_0),
+.vss_testmode(REGULATIONtestmode_5fb9e860_1),
 .FB_REGULATION(FB_REGULATION),
 .ok_regulation(ok_regulation),
 .VSS_REGULATION(VSS_REGULATION),
 .enable_regulation(enable_regulation),
-.HIJACK_VSS_REGULATION(net_76),
-.hijack_enable_regulation(net_74)
+.HIJACK_VSS_REGULATION(net_143),
+.hijack_enable_regulation(net_141)
 );
 
 REGULATIONwaltzMAIN XMAIN (
-.VC(net_75),
-.tmi(tmi[4:0]),
+.VC(net_142),
 .MUDG(MUDG),
 .MUDV(MUDV),
-.REFINT(net_73),
-.CZCOMP_0(net_52),
-.CZCOMP_1(net_53),
-.CZCOMP_2(net_54),
-.RZCOMP_0(net_55),
-.RZCOMP_1(net_56),
-.RZCOMP_2(net_57),
+.REFINT(net_140),
+.CZCOMP_0(REGULATIONczcomp_97e82c27_0),
+.CZCOMP_1(REGULATIONczcomp_97e82c27_1),
+.CZCOMP_2(REGULATIONczcomp_97e82c27_2),
+.RZCOMP_0(REGULATIONrzcomp_166fa6ed_0),
+.RZCOMP_1(REGULATIONrzcomp_166fa6ed_1),
+.RZCOMP_2(REGULATIONrzcomp_166fa6ed_2),
 .CELG59462(CELG59462),
 .CELV96848(CELV96848),
 .go_driver(go_driver),
-.GAINCOMP_0(net_58),
-.GAINCOMP_1(net_59),
+.GAINCOMP_0(REGULATIONgaincomp_b6f2b1a6_0),
+.GAINCOMP_1(REGULATIONgaincomp_b6f2b1a6_1),
 .CELSUB40948(CELSUB40948),
-.DCMRIPPLE_0(net_50),
-.DCMRIPPLE_1(net_51),
+.DCMRIPPLE_0(REGULATIONripple_668a78c0_0),
+.DCMRIPPLE_1(REGULATIONripple_668a78c0_1),
+.IP_0d0816e7(IP_0d0816e7),
 .IP_70133221(IP_70133221),
 .IP_96171c99(IP_96171c99),
-.IP_df440ad3(IP_df440ad3),
 .IP_f5d94fdd(IP_f5d94fdd),
 .IP_fa70de6e(IP_fa70de6e),
 .IREF_DRIVER(IREF_DRIVER),
@@ -169,9 +193,68 @@ REGULATIONwaltzMAIN XMAIN (
 .FB_REGULATION(FB_REGULATION),
 .ok_regulation(ok_regulation),
 .REF_REGULATION(REF_REGULATION),
-.VSS_REGULATION(net_76),
+.VSS_REGULATION(net_143),
 .CELREF_e3b0c442(CELREF_e3b0c442),
-.enable_regulation(net_74)
+.enable_regulation(net_141),
+.trim_clamp_0d0816e7(trim_clamp_0d0816e7[7:0]),
+.trim_gm_negative_70133221(trim_gm_negative_70133221[6:0]),
+.trim_gm_positive_70133221(trim_gm_positive_70133221[6:0]),
+.trim_vbuffer_negative_96171c99(trim_vbuffer_negative_96171c99[6:0]),
+.trim_vbuffer_positive_96171c99(trim_vbuffer_positive_96171c99[6:0])
+);
+
+drm56 drm_hex0x01 (
+.G(CELG59462),
+.V(CELV96848),
+.d0(a0),
+.d1(a1),
+.id({a0,a0,a0,a0,a0,a0,a0,a1}),
+.SUB(CELSUB40948),
+.tmi(tmi[4:0]),
+.drm0({trim_clamp_0d0816e7[7],trim_clamp_0d0816e7[6],trim_clamp_0d0816e7[5],trim_clamp_0d0816e7[4],trim_clamp_0d0816e7[3],trim_clamp_0d0816e7[2],trim_clamp_0d0816e7[1],trim_clamp_0d0816e7[0]}),
+.drm1({noconn_drm56_drm1_7,trim_vbuffer_negative_96171c99[6],trim_vbuffer_negative_96171c99[5],trim_vbuffer_negative_96171c99[4],trim_vbuffer_negative_96171c99[3],trim_vbuffer_negative_96171c99[2],trim_vbuffer_negative_96171c99[1],trim_vbuffer_negative_96171c99[0]}),
+.drm2({noconn_drm56_drm2_7,trim_vbuffer_positive_96171c99[6],trim_vbuffer_positive_96171c99[5],trim_vbuffer_positive_96171c99[4],trim_vbuffer_positive_96171c99[3],trim_vbuffer_positive_96171c99[2],trim_vbuffer_positive_96171c99[1],trim_vbuffer_positive_96171c99[0]}),
+.drm3({noconn_drm56_drm3_7,trim_gm_negative_70133221[6],trim_gm_negative_70133221[5],trim_gm_negative_70133221[4],trim_gm_negative_70133221[3],trim_gm_negative_70133221[2],trim_gm_negative_70133221[1],trim_gm_negative_70133221[0]}),
+.drm4({noconn_drm56_drm4_7,trim_gm_positive_70133221[6],trim_gm_positive_70133221[5],trim_gm_positive_70133221[4],trim_gm_positive_70133221[3],trim_gm_positive_70133221[2],trim_gm_positive_70133221[1],trim_gm_positive_70133221[0]}),
+.drm5({REGULATIONczcomp_97e82c27_2,REGULATIONczcomp_97e82c27_1,REGULATIONczcomp_97e82c27_0,REGULATIONripple_668a78c0_1,REGULATIONripple_668a78c0_0,REGULATIONtestmode_5fb9e860_2,REGULATIONtestmode_5fb9e860_1,REGULATIONtestmode_5fb9e860_0}),
+.drm6({noconn_drm56_drm6_7,noconn_drm56_drm6_6,noconn_drm56_drm6_5,REGULATIONgaincomp_b6f2b1a6_1,REGULATIONgaincomp_b6f2b1a6_0,REGULATIONrzcomp_166fa6ed_2,REGULATIONrzcomp_166fa6ed_1,REGULATIONrzcomp_166fa6ed_0}),
+.por0({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por1({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por2({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por3({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por4({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por5({a0,a0,a0,a0,a1,a0,a0,a0}),
+.por6({a0,a0,a0,a0,a1,a0,a1,a1}),
+.bypload(a0),
+.lastdrm(a0)
+);
+
+STONEnoconn XNCnoconn_drm56_drm1_7 (
+.noconn(noconn_drm56_drm1_7)
+);
+
+STONEnoconn XNCnoconn_drm56_drm2_7 (
+.noconn(noconn_drm56_drm2_7)
+);
+
+STONEnoconn XNCnoconn_drm56_drm3_7 (
+.noconn(noconn_drm56_drm3_7)
+);
+
+STONEnoconn XNCnoconn_drm56_drm4_7 (
+.noconn(noconn_drm56_drm4_7)
+);
+
+STONEnoconn XNCnoconn_drm56_drm6_5 (
+.noconn(noconn_drm56_drm6_5)
+);
+
+STONEnoconn XNCnoconn_drm56_drm6_6 (
+.noconn(noconn_drm56_drm6_6)
+);
+
+STONEnoconn XNCnoconn_drm56_drm6_7 (
+.noconn(noconn_drm56_drm6_7)
 );
 
 endmodule
