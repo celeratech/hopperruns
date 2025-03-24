cea2120fe63ed320743612ed449702b848f2e1fb
Hirshol Pheir <hirshol@gmail.com>
2025-03-24 22:53:50 +0000

    hopper run INCURRENTSENSEpuget_20250324.22.53.50

---
+++ INCURRENTSENSEpuget/verilog8/CURRENTSENSEinMAIN 2.v
@@ -0,0 +1,473 @@
+// ------------------------ Module Definitions -----------
+module VESPAasmINPUT2 (o,i0,i1,Tstate,CELG59462,CELV96848,CELSUB40948);
+  output  o;
+  input  i0;
+  input  i1;
+  input  Tstate;
+  input  CELG59462;
+  input  CELV96848;
+  input  CELSUB40948;
+endmodule
+
+module VESPAasmINPUT1 (o,i0,Tstate,CELG59462,CELV96848,CELSUB40948);
+  output  o;
+  input  i0;
+  input  Tstate;
+  input  CELG59462;
+  input  CELV96848;
+  input  CELSUB40948;
+endmodule
+
+module VESPAdftpulse (stop,pulse,start,CELG59462,CELV96848,CELSUB40948);
+  input  stop;
+  output  pulse;
+  input  start;
+  input  CELG59462;
+  input  CELV96848;
+  input  CELSUB40948;
+endmodule
+
+//Celera:dbuf_e926e395
+//Celera Confidential Symbol Generator
+//Digital Buffer
+module dbuf_e926e395 (CELV,CELG,i,o,SUB);
+input CELV;
+input CELG;
+input i;
+input SUB;
+output o;
+endmodule
+
+
+
+//Celera:currentsenseamplifier_48054596
+//Celera Confidential Symbol Generator
+//Sense Type: resistor, Usage: n, Max Input Voltage: 40V, Max Sense Voltage: 0.07V
+//Output Current: 20uA, Output Voltage: 2.4375V, Output Type: voltage, Accuracy: yes, DFT: no
+module currentsenseamplifier_48054596 (enable_currentsenseamplifier,IP,ok_currentsenseamplifier,
+INP_CURRENTSENSEAMPLIFIER,INN_CURRENTSENSEAMPLIFIER,OUT_CURRENTSENSEAMPLIFIER,
+CELV,
+RTN,
+trim_csaoutput,
+CELPOS,
+trim_csainput,
+CELSUB,CELG);
+input CELV;
+input INP_CURRENTSENSEAMPLIFIER;
+input INN_CURRENTSENSEAMPLIFIER;
+input IP;
+input enable_currentsenseamplifier;
+output ok_currentsenseamplifier;
+output OUT_CURRENTSENSEAMPLIFIER;
+input [6:0] trim_csaoutput;
+inout RTN;
+input CELPOS;
+input [6:0] trim_csainput;
+input CELSUB;
+input CELG;
+endmodule
+
+
+
+//Verilog HDL for "PEBBLES", "PEBBLEtielo" "functional"
+
+
+module PEBBLEtielo ( q, G, SUB, V );
+
+  input V;
+  output q;
+  input G;
+  input SUB;
+endmodule
+
+
+//Celera:delayclock_58885d25
+//CONTROL:Pin:4 outputs
+module delayclock_58885d25 (in,CELV,out,clock,celeraporb,
+delay,
+CELG,CELSUB);
+input CELV;
+input in;
+output out;
+input clock;
+input celeraporb;
+input CELSUB;
+input CELG;
+input [1:0] delay;
+endmodule
+
+
+
+//Celera:switchtswitch_4b165e0d
+//Celera Confidential Symbol Generator
+//1000 Ohm tswitchSwitch
+module switchtswitch_4b165e0d (CELV,O,I,enable_switch,CELG,CELSUB);
+input CELV;
+inout I;
+input enable_switch;
+inout O;
+input CELG;
+input CELSUB;
+endmodule
+
+
+
+//Celera:resistor_f78b97c7
+//Celera Confidential Symbol Generator
+//RESISTOR:400.00KOhm TYPE:poly Adjust:240.00Kohm DFT:no
+module resistor_f78b97c7 (RP,
+CELV,
+CELG,
+CELSUB,
+factory_adjust_resistor,
+RN);
+inout RP;
+inout RN;
+input CELV;
+input CELG;
+input CELSUB;
+input [2:0] factory_adjust_resistor;
+endmodule
+
+
+
+//Celera:resistor_308d63e4
+//Celera Confidential Symbol Generator
+//RESISTOR:400.00KOhm TYPE:poly Adjust:240.00Kohm DFT:no
+module resistor_308d63e4 (RP,
+CELV,
+CELG,
+CELSUB,
+factory_adjust_resistor,
+RN);
+inout RP;
+inout RN;
+input CELV;
+input CELG;
+input CELSUB;
+input [2:0] factory_adjust_resistor;
+endmodule
+
+
+
+//Celera:amplifier_b9b34961
+//Celera Confidential Symbol Generator
+//Gain Adjust:openloop, Input Type:p, Bandwidth:low
+module amplifier_b9b34961 (SIMPV,INP,IP,OUT,enable_amplifier,ok_amplifier,global_amplifier,
+INN,
+CELG,CELSUB);
+input SIMPV;
+input INP;
+input INN;
+input IP;
+output OUT;
+input enable_amplifier;
+output ok_amplifier;
+input global_amplifier;
+input CELG;
+input CELSUB;
+endmodule
+
+
+
+//Celera:capacitorfixed_bb7f99ed
+//Celera Confidential Symbol Generator
+//CAPACITOR CONTROL:capacitor
+//VALUE: 14.15pF TYPE:mim
+module capacitorfixed_bb7f99ed (CP,
+CN);
+inout CP;
+inout CN;
+endmodule
+
+
+
+//Celera:capacitorfixed_b8fedd7c
+//Celera Confidential Symbol Generator
+//CAPACITOR CONTROL:capacitor
+//VALUE: 28.30pF TYPE:mim
+module capacitorfixed_b8fedd7c (CP,
+CN);
+inout CP;
+inout CN;
+endmodule
+
+
+
+//Verilog HDL for "DRM", "drm24" "functional"
+
+
+module drm24 ( V, G, SUB, tmi, bypload, lastdrm, id, por0, por1, por2, drm0,
+drm1, drm2, d1, d0 );
+
+  input lastdrm;
+  input V;
+  output d1;
+  input  [7:0] id;
+  output d0;
+  output  [7:0] drm2;
+  input  [7:0] por2;
+  input  [7:0] por1;
+  input bypload;
+  output  [7:0] drm0;
+  input  [7:0] por0;
+  input G;
+  output  [7:0] drm1;
+  inout  [4:0] tmi;
+  input SUB;
+endmodule
+
+
+//Verilog HDL for "Generate", "STONEnoconn" "functional"
+
+
+module STONEnoconn ( noconn );
+
+  input noconn;
+endmodule
+
+
+//Verilog HDL for "Generate", "WRAPPER1" "functional"
+
+
+module WRAPPER1 ( o, i );
+
+  input i;
+  output o;
+endmodule
+
+
+// ------------------------ Module Verilog ---------------
+module CURRENTSENSEinMAIN (IIN, tmi, SIMPV, VOUTSN, VOUTSP, dft_ok, ok_iin, CELG59462, CELV96848, PORB97836, CELSUB40948, IP_4ad46a6e, IP_5c672501, dft_startup, IIN_TELEMETRY, ok_currentsense, dft_measure_delay, clock_currentsense, enable_currentsense, measure_currentsense, kelvin_GNDcurrentsense, register_CURRENTSENSEinMEASUREdelay_0);
+inout  IIN;
+inout [4:0] tmi;
+input  SIMPV;
+input  VOUTSN;
+input  VOUTSP;
+output  dft_ok;
+output  ok_iin;
+input  CELG59462;
+input  CELV96848;
+input  PORB97836;
+input  CELSUB40948;
+input  IP_4ad46a6e;
+input  IP_5c672501;
+output  dft_startup;
+inout  IIN_TELEMETRY;
+output  ok_currentsense;
+output  dft_measure_delay;
+input  clock_currentsense;
+input  enable_currentsense;
+input  measure_currentsense;
+inout  kelvin_GNDcurrentsense;
+input [1:0] register_CURRENTSENSEinMEASUREdelay_0;
+
+
+// ------------------------ Wires ------------------------
+wire [4:0] tmi;
+wire [1:0] register_CURRENTSENSEinMEASUREdelay_0;
+wire [6:0] trim_csainput;
+wire [6:0] trim_csaoutput;
+wire [1:0] delay;
+wire [2:0] factory_adjust_resistor;
+wire [7:0] id;
+wire [7:0] drm0;
+wire [7:0] drm1;
+wire [7:0] drm2;
+wire [7:0] por0;
+wire [7:0] por1;
+wire [7:0] por2;
+
+// ------------------------ Networks ---------------------
+VESPAasmINPUT2 XU3 (
+.o(ok_currentsense),
+.i0(net_132),
+.i1(net_130),
+.Tstate(enable_currentsense),
+.CELG59462(CELG59462),
+.CELV96848(CELV96848),
+.CELSUB40948(CELSUB40948)
+);
+
+VESPAasmINPUT1 XU5 (
+.o(net_135),
+.i0(clock_currentsense),
+.Tstate(enable_currentsense),
+.CELG59462(CELG59462),
+.CELV96848(CELV96848),
+.CELSUB40948(CELSUB40948)
+);
+
+VESPAdftpulse XU6 (
+.stop(net_131),
+.pulse(dft_measure_delay),
+.start(measure_currentsense),
+.CELG59462(CELG59462),
+.CELV96848(CELV96848),
+.CELSUB40948(CELSUB40948)
+);
+
+VESPAasmINPUT1 XU7 (
+.o(net_134),
+.i0(measure_currentsense),
+.Tstate(ok_currentsense),
+.CELG59462(CELG59462),
+.CELV96848(CELV96848),
+.CELSUB40948(CELSUB40948)
+);
+
+VESPAdftpulse XU8 (
+.stop(ok_currentsense),
+.pulse(dft_startup),
+.start(enable_currentsense),
+.CELG59462(CELG59462),
+.CELV96848(CELV96848),
+.CELSUB40948(CELSUB40948)
+);
+
+dbuf_e926e395 XU4 (
+.i(net_131),
+.o(ok_iin),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU11 (
+.i(ok_currentsense),
+.o(dft_ok),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+currentsenseamplifier_48054596 XCSA1 (
+.IP(IP_5c672501),
+.RTN(kelvin_GNDcurrentsense),
+.CELG(CELG59462),
+.CELV(CELV96848),
+.CELPOS(VOUTSP),
+.CELSUB(CELSUB40948),
+.trim_csainput({trim_csainput_5c672501_6,trim_csainput_5c672501_5,trim_csainput_5c672501_4,trim_csainput_5c672501_3,trim_csainput_5c672501_2,trim_csainput_5c672501_1,trim_csainput_5c672501_0}),
+.trim_csaoutput({trim_csaoutput_5c672501_6,trim_csaoutput_5c672501_5,trim_csaoutput_5c672501_4,trim_csaoutput_5c672501_3,trim_csaoutput_5c672501_2,trim_csaoutput_5c672501_1,trim_csaoutput_5c672501_0}),
+.ok_currentsenseamplifier(net_130),
+.INN_CURRENTSENSEAMPLIFIER(VOUTSP),
+.INP_CURRENTSENSEAMPLIFIER(VOUTSN),
+.OUT_CURRENTSENSEAMPLIFIER(IIN),
+.enable_currentsenseamplifier(enable_currentsense)
+);
+
+PEBBLEtielo XtieLo (
+.G(CELG59462),
+.V(CELV96848),
+.q(tl0),
+.SUB(CELSUB40948)
+);
+
+delayclock_58885d25 Xdelay1 (
+.in(net_134),
+.out(net_131),
+.CELG(CELG59462),
+.CELV(CELV96848),
+.clock(net_135),
+.delay({net_86,net_85}),
+.CELSUB(CELSUB40948),
+.celeraporb(PORB97836)
+);
+
+switchtswitch_4b165e0d Xswitch1 (
+.I(net_133),
+.O(IIN_TELEMETRY),
+.CELG(CELG59462),
+.CELV(CELV96848),
+.CELSUB(CELSUB40948),
+.enable_switch(measure_currentsense)
+);
+
+resistor_f78b97c7 Xresistor1 (
+.RN(IIN),
+.RP(net_69),
+.CELG(CELG59462),
+.CELV(CELV96848),
+.CELSUB(CELSUB40948),
+.factory_adjust_resistor({factory_adjust_resistor_c67dbca4_2,factory_adjust_resistor_c67dbca4_1,factory_adjust_resistor_c67dbca4_0})
+);
+
+resistor_308d63e4 Xresistor2 (
+.RN(net_69),
+.RP(net_76),
+.CELG(CELG59462),
+.CELV(CELV96848),
+.CELSUB(CELSUB40948),
+.factory_adjust_resistor({factory_adjust_resistor_df617a62_2,factory_adjust_resistor_df617a62_1,factory_adjust_resistor_df617a62_0})
+);
+
+amplifier_b9b34961 Xamplifier1 (
+.IP(IP_4ad46a6e),
+.INN(net_133),
+.INP(net_76),
+.OUT(net_133),
+.CELG(CELG59462),
+.SIMPV(SIMPV),
+.CELSUB(CELSUB40948),
+.ok_amplifier(net_132),
+.enable_amplifier(enable_currentsense),
+.global_amplifier(tl0)
+);
+
+capacitorfixed_bb7f99ed Xcapacitor1 (
+.CN(kelvin_GNDcurrentsense),
+.CP(net_76)
+);
+
+capacitorfixed_b8fedd7c Xcapacitor2 (
+.CN(net_69),
+.CP(net_133)
+);
+
+drm24 drm_hex0x01 (
+.G(CELG59462),
+.V(CELV96848),
+.d0(a0),
+.d1(a1),
+.id({a0,a0,a0,a0,a0,a0,a0,a1}),
+.SUB(CELSUB40948),
+.tmi(tmi[4:0]),
+.drm0({noconn_drm24_drm0_7,trim_csainput_5c672501_6,trim_csainput_5c672501_5,trim_csainput_5c672501_4,trim_csainput_5c672501_3,trim_csainput_5c672501_2,trim_csainput_5c672501_1,trim_csainput_5c672501_0}),
+.drm1({noconn_drm24_drm1_7,trim_csaoutput_5c672501_6,trim_csaoutput_5c672501_5,trim_csaoutput_5c672501_4,trim_csaoutput_5c672501_3,trim_csaoutput_5c672501_2,trim_csaoutput_5c672501_1,trim_csaoutput_5c672501_0}),
+.drm2({noconn_drm24_drm2_7,noconn_drm24_drm2_6,factory_adjust_resistor_df617a62_2,factory_adjust_resistor_df617a62_1,factory_adjust_resistor_df617a62_0,factory_adjust_resistor_c67dbca4_2,factory_adjust_resistor_c67dbca4_1,factory_adjust_resistor_c67dbca4_0}),
+.por0({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por1({a0,a0,a0,a0,a0,a0,a0,a0}),
+.por2({a0,a0,a0,a0,a0,a0,a0,a0}),
+.bypload(a0),
+.lastdrm(a0)
+);
+
+STONEnoconn XNCnoconn_drm24_drm0_7 (
+.noconn(noconn_drm24_drm0_7)
+);
+
+STONEnoconn XNCnoconn_drm24_drm1_7 (
+.noconn(noconn_drm24_drm1_7)
+);
+
+STONEnoconn XNCnoconn_drm24_drm2_6 (
+.noconn(noconn_drm24_drm2_6)
+);
+
+STONEnoconn XNCnoconn_drm24_drm2_7 (
+.noconn(noconn_drm24_drm2_7)
+);
+
+WRAPPER1 XWRAPregister_CURRENTSENSEinMEASUREdelay_0_0 (
+.i(register_CURRENTSENSEinMEASUREdelay_0[0]),
+.o(net_85)
+);
+
+WRAPPER1 XWRAPregister_CURRENTSENSEinMEASUREdelay_0_1 (
+.i(register_CURRENTSENSEinMEASUREdelay_0[1]),
+.o(net_86)
+);
+
+endmodule
+
---
+++ WALTZ/verilog4/fet_fetdn_a0946f8e_Xfet 2.v
@@ -0,0 +1,75 @@
+// Celera Brick Generator Confidential
+//CORE:singlepowerfetN
+//NAME:fet_fetdn_a0946f8e_Xfet
+//GENERATOR REVISION:0.3.9
+//FET TYPE:n
+//ON RESISTANCE:0.820 Ohms
+//VDS RATING:60V
+//VGS RATING:6V
+//BODY DIODE:yes
+//DIODE DRIVE:diode
+//REPLICA:no
+//REPLICA GAIN:10
+//KEVLIN:no
+//DFT:no
+//ROTATE:no
+
+//Celera Confidential Do Not Copy NMOS
+module an5g60dw2_160p1x0p5x44p0x1p0 (DRAIN,GATE,SOURCE,SUB);
+input GATE;
+input SUB;
+inout SOURCE;
+inout DRAIN;
+endmodule
+
+//Celera Confidential Do Not Copy fet_fetdn_a0946f8e_Xfet
+//Celera Confidential Symbol Generator
+//power NMOS:Ron:0.820 Ohm
+//Vgs 6V Vds 60V
+//Kelvin:no
+
+module fet_fetdn_a0946f8e_Xfet (GATE,SOURCE,DRAIN,SUB);
+input GATE;
+inout SOURCE;
+inout DRAIN;
+input SUB;
+
+//Celera Confidential Do Not Copy an5g60dw2_160p1x0p5x44p0x1p0
+an5g60dw2_160p1x0p5x44p0x1p0 Xnmos0(
+.DRAIN (DRAIN),
+.GATE (GATE),
+.SOURCE (SOURCE),
+.SUB (SUB)
+);
+//,diesize,an5g60dw2_160p1x0p5x44p0x1p0
+
+//Celera Confidential Do Not Copy an5g60dw2_160p1x0p5x44p0x1p0
+an5g60dw2_160p1x0p5x44p0x1p0 Xnmos1(
+.DRAIN (DRAIN),
+.GATE (GATE),
+.SOURCE (SOURCE),
+.SUB (SUB)
+);
+//,diesize,an5g60dw2_160p1x0p5x44p0x1p0
+
+//Celera Confidential Do Not Copy an5g60dw2_160p1x0p5x44p0x1p0
+an5g60dw2_160p1x0p5x44p0x1p0 Xnmos2(
+.DRAIN (DRAIN),
+.GATE (GATE),
+.SOURCE (SOURCE),
+.SUB (SUB)
+);
+//,diesize,an5g60dw2_160p1x0p5x44p0x1p0
+
+//Celera Confidential Do Not Copy an5g60dw2_160p1x0p5x44p0x1p0
+an5g60dw2_160p1x0p5x44p0x1p0 Xnmos3(
+.DRAIN (DRAIN),
+.GATE (GATE),
+.SOURCE (SOURCE),
+.SUB (SUB)
+);
+//,diesize,an5g60dw2_160p1x0p5x44p0x1p0
+
+//Celera Confidential Do Not Copy Module End
+//Celera Schematic Generator
+endmodule
