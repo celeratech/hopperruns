cb9a8f288da218c4724e26f920ed96ce28df703a
Hirshol Pheir <hirshol@gmail.com>
2025-03-25 22:34:01 +0000

    hopper run CAPcalculator_20250325.22.34.00

--- CAPcalculator/verilog10/CAPcalculatorCONFIGURATION.v
+++ CAPcalculator/verilog10/CAPcalculatorCONFIGURATION.v
@@ -144,6 +144,7 @@ wire [15:0] register_vshunt_15;
 wire [15:0] a;
 wire [15:0] b;
 wire [15:0] o;
+wire [15:0] vshunt_15;
 
 // ------------------------ Networks ---------------------
 PEBBLEtiehi XDRMNOTH (
--- CAPcalculator/verilog11/FORCE_CAPcalculator.v
+++ CAPcalculator/verilog11/FORCE_CAPcalculator.v
@@ -99,7 +99,7 @@ input GND;
 endmodule 
 
 // ------------------------ Module Verilog ---------------
-module FORCE_CAPcalculator (GND, porb, SIMPV, CELG59462, CELV96848, PORB97836, power_fail, CELSUB40948, IP_9a4148db, meas_vcap1_0, meas_vcap1_1, meas_vcap1_2, meas_vcap1_3, meas_vcap1_4, meas_vcap1_5, meas_vcap1_6, meas_vcap1_7, meas_vcap1_8, meas_vcap1_9, meas_vcap2_0, meas_vcap2_1, meas_vcap2_2, meas_vcap2_3, meas_vcap2_4, meas_vcap2_5, meas_vcap2_6, meas_vcap2_7, meas_vcap2_8, meas_vcap2_9, meas_vcap3_0, meas_vcap3_1, meas_vcap3_2, meas_vcap3_3, meas_vcap3_4, meas_vcap3_5, meas_vcap3_6, meas_vcap3_7, meas_vcap3_8, meas_vcap3_9, meas_vcap4_0, meas_vcap4_1, meas_vcap4_2, meas_vcap4_3, meas_vcap4_4, meas_vcap4_5, meas_vcap4_6, meas_vcap4_7, meas_vcap4_8, meas_vcap4_9, meas_vcap1_10, meas_vcap1_11, meas_vcap1_12, meas_vcap1_13, meas_vcap1_14, meas_vcap1_15, meas_vcap2_10, meas_vcap2_11, meas_vcap2_12, meas_vcap2_13, meas_vcap2_14, meas_vcap2_15, meas_vcap3_10, meas_vcap3_11, meas_vcap3_12, meas_vcap3_13, meas_vcap3_14, meas_vcap3_15, meas_vcap4_10, meas_vcap4_11, meas_vcap4_12, meas_vcap4_13, meas_vcap4_14, meas_vcap4_15, clock_calculator, enable_capcalculate, kelvin_GNDcalculator);
+module FORCE_CAPcalculator (GND, porb, SIMPV, CELG59462, CELV96848, PORB97836, power_fail, CELSUB40948, IP_9a4148db, meas_vcap1_0, meas_vcap1_1, meas_vcap1_2, meas_vcap1_3, meas_vcap1_4, meas_vcap1_5, meas_vcap1_6, meas_vcap1_7, meas_vcap1_8, meas_vcap1_9, meas_vcap2_0, meas_vcap2_1, meas_vcap2_2, meas_vcap2_3, meas_vcap2_4, meas_vcap2_5, meas_vcap2_6, meas_vcap2_7, meas_vcap2_8, meas_vcap2_9, meas_vcap3_0, meas_vcap3_1, meas_vcap3_2, meas_vcap3_3, meas_vcap3_4, meas_vcap3_5, meas_vcap3_6, meas_vcap3_7, meas_vcap3_8, meas_vcap3_9, meas_vcap4_0, meas_vcap4_1, meas_vcap4_2, meas_vcap4_3, meas_vcap4_4, meas_vcap4_5, meas_vcap4_6, meas_vcap4_7, meas_vcap4_8, meas_vcap4_9, meas_vcap1_10, meas_vcap1_11, meas_vcap1_12, meas_vcap1_13, meas_vcap1_14, meas_vcap1_15, meas_vcap2_10, meas_vcap2_11, meas_vcap2_12, meas_vcap2_13, meas_vcap2_14, meas_vcap2_15, meas_vcap3_10, meas_vcap3_11, meas_vcap3_12, meas_vcap3_13, meas_vcap3_14, meas_vcap3_15, meas_vcap4_10, meas_vcap4_11, meas_vcap4_12, meas_vcap4_13, meas_vcap4_14, meas_vcap4_15, clock_calculator, enable_capcalculate, kelvin_GNDcalculator, register_vshunt_15[0], register_vshunt_15[1], register_vshunt_15[2], register_vshunt_15[3], register_vshunt_15[4], register_vshunt_15[5], register_vshunt_15[6], register_vshunt_15[7], register_vshunt_15[8], register_vshunt_15[9], register_vshunt_15[10], register_vshunt_15[11], register_vshunt_15[12], register_vshunt_15[13], register_vshunt_15[14], register_vshunt_15[15]);
 inout  GND;
 output  porb;
 output  SIMPV;
@@ -176,6 +176,22 @@ inout  meas_vcap4_15;
 output  clock_calculator;
 output  enable_capcalculate;
 output  kelvin_GNDcalculator;
+inout  register_vshunt_15[0];
+inout  register_vshunt_15[1];
+inout  register_vshunt_15[2];
+inout  register_vshunt_15[3];
+inout  register_vshunt_15[4];
+inout  register_vshunt_15[5];
+inout  register_vshunt_15[6];
+inout  register_vshunt_15[7];
+inout  register_vshunt_15[8];
+inout  register_vshunt_15[9];
+inout  register_vshunt_15[10];
+inout  register_vshunt_15[11];
+inout  register_vshunt_15[12];
+inout  register_vshunt_15[13];
+inout  register_vshunt_15[14];
+inout  register_vshunt_15[15];
 
 
 // ------------------------ Wires ------------------------
@@ -279,82 +295,82 @@ vpwl_0x0_10d998ux0_11d0ux0 V_power_failx (
 );
 
 vpwl_0x0_0d999ux0_1d0ux5 Xdatamap1_pwl_bit0 (
-.PLUS(meas_vcap3_10),
+.PLUS(register_vshunt_15[0]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit1 (
-.PLUS(meas_vcap3_11),
+.PLUS(register_vshunt_15[1]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit2 (
-.PLUS(meas_vcap3_12),
+.PLUS(register_vshunt_15[2]),
 .MINUS(GND)
 );
 
 vpwl_0x0_0d999ux0_1d0ux5 Xdatamap1_pwl_bit3 (
-.PLUS(meas_vcap3_13),
+.PLUS(register_vshunt_15[3]),
 .MINUS(GND)
 );
 
 vpwl_0x0_0d999ux0_1d0ux5 Xdatamap1_pwl_bit4 (
-.PLUS(meas_vcap3_14),
+.PLUS(register_vshunt_15[4]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit5 (
-.PLUS(meas_vcap3_15),
+.PLUS(register_vshunt_15[5]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit6 (
-.PLUS(meas_vcap2_0),
+.PLUS(register_vshunt_15[6]),
 .MINUS(GND)
 );
 
 vpwl_0x0_0d999ux0_1d0ux5 Xdatamap1_pwl_bit7 (
-.PLUS(meas_vcap2_1),
+.PLUS(register_vshunt_15[7]),
 .MINUS(GND)
 );
 
 vpwl_0x0_0d999ux0_1d0ux5 Xdatamap1_pwl_bit8 (
-.PLUS(meas_vcap2_2),
+.PLUS(register_vshunt_15[8]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit9 (
-.PLUS(meas_vcap2_3),
+.PLUS(register_vshunt_15[9]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit10 (
-.PLUS(meas_vcap2_4),
+.PLUS(register_vshunt_15[10]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit11 (
-.PLUS(meas_vcap2_5),
+.PLUS(register_vshunt_15[11]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit12 (
-.PLUS(meas_vcap2_6),
+.PLUS(register_vshunt_15[12]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit13 (
-.PLUS(meas_vcap2_7),
+.PLUS(register_vshunt_15[13]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit14 (
-.PLUS(meas_vcap2_8),
+.PLUS(register_vshunt_15[14]),
 .MINUS(GND)
 );
 
 vpwl_0x0_1d0ux0 Xdatamap1_pwl_bit15 (
-.PLUS(meas_vcap2_9),
+.PLUS(register_vshunt_15[15]),
 .MINUS(GND)
 );
 
