1d94c81c7575bddd6584466774d298080b8be301
Hirshol Pheir <hirshol@gmail.com>
2025-03-31 20:58:08 +0000

    hopper run REGULATIONreference_20250331.20.58.08

--- REGULATIONreference/verilog5/switchpulldown_2b651f4f.v
+++ REGULATIONreference/verilog5/switchpulldown_2b651f4f.v
@@ -35,6 +35,19 @@ module PEBBLEinv1 ( o, G, SUB, V, i );
 endmodule
 
 //Celera Confidential Do Not Copy fet_switchpulldown_2b651f4f_Xnmos0.v
+//Celera:fet_switchpulldown_2b651f4f_Xnmos0
+//Celera Confidential Symbol Generator
+//signal NMOS:Ron:10 Ohm
+//Vgs 6V Vds 6V
+//Kelvin:no
+module fet_switchpulldown_2b651f4f_Xnmos0 (GATE,SOURCE,DRAIN,NMOSiso6,SUB);
+input GATE;
+inout SOURCE;
+inout DRAIN;
+input SUB;
+input NMOSiso6;
+endmodule
+
 
 //Celera Confidential Do Not Copy switchpulldown_2b651f4f
 //Celera Confidential Symbol Generator
