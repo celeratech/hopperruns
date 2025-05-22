c3b5881f7f747a587ad5b5262c4712edd4d9d0a5
Hirshol Pheir <hirshol@gmail.com>
2025-05-22 23:25:37 +0000

    hopper run CAPtest_20250522.23.25.36

--- CAPtest/verilog11/FORCEcaptest.v
+++ CAPtest/verilog11/FORCEcaptest.v
@@ -21,73 +21,53 @@ module STONEnoconn ( noconn );
 endmodule
 
 
-module vpwl_3578e779_0x0(PLUS, MINUS);
+module vpwl__0x0(PLUS, MINUS);
 	output PLUS;
 	input MINUS;
 endmodule
 
 
-module vpwl_1d1997d9_0x0_10d0ux0_11d0ux5d0 (PLUS,MINUS);
+module vpwl__0x0_10d0ux0_11d0ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_8dee584c_0x0_11ux0_11d001ux5d0 (PLUS,MINUS);
+module vpwl__0x0_11ux0_11d001ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_97ff8a3b_0x0 (PLUS,MINUS);
+module vpwl__0x0_15ux0_15d001ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_36eb942a_0x0_15ux0_15d001ux5d0 (PLUS,MINUS);
+module vpwl__0x0_6ux0_6d001ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_b36cfd62_0x0_6ux0_6d001ux5d0 (PLUS,MINUS);
+module vpwl__0x0_10ux0_10d001ux0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_b5c9a941_0x0_10ux0_10d001ux0 (PLUS,MINUS);
+module vpwl__0x0_6ux0_7ux12 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_4fa18aab_0x0_6ux0_7ux12 (PLUS,MINUS);
+module vpwl__0x0_7ux0_7d001ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_72a1ee49_0x0_7ux0_7d001ux5d0 (PLUS,MINUS);
+module vpwl__0x0_20ux0_20d001ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_78dcda0d_0x0_6ux0_6d001ux5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_4f04898e_0x0_20ux0_20d001ux5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_0acdff5b_0x0_10ux0_10d001ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_b97f08a7_0x0_6ux0_6d001ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_01db9d2b_0x0_6ux0_6d001ux0 (PLUS,MINUS);
+module vpwl__0x0_6ux0_6d001ux0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
@@ -118,332 +98,27 @@ module vpulse_0x5d0x10d0ux10ux1nx1nx5u (PLUS,MINUS);
   input  MINUS;
 endmodule
 
-module vpwl_b7bd27de_0x0_150ux0 (PLUS,MINUS);
+module vpwl_0x0_150ux0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_6df1465e_0x5d0_149d999ux5d0_150ux0 (PLUS,MINUS);
+module vpwl_0x5d0_149d999ux5d0_150ux0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_d2c11a42_0x0_150ux0 (PLUS,MINUS);
+module vpwl_0x0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_4d79b1f7_0x0_150ux0 (PLUS,MINUS);
+module vpwl_0x5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
 
-module vpwl_2302ebc2_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_8932f903_0x5d0_149d999ux5d0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_2e687a9e_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_bb48c2c6_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_cfef51f0_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_b1459dd9_0x5d0_149d999ux5d0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_dd874091_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_4c73ed85_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_ac798407_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_a6b7f6ba_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_b845f025_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_749b025f_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_e67b28ba_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_6bf14cb7_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_325aed86_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_f26adafd_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_6219f260_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_6d3fb9d6_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_537cdc2a_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_ebdc8581_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_f2738915_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_027ae12a_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_bd4661d6_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_556394c1_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_48f2086c_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_b9d4ba9f_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_304d6518_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_4075232a_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_75e85800_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_dab7e819_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_f1d31cca_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_1e3f9d92_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_3bae73ea_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_f10feb5f_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_63779624_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_125717fe_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_eed155ff_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_a9867611_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_afd00360_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_62f58745_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_76288a8e_0x0_149d999ux0_150ux5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_7139535e_0x5d0_149d999ux5d0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_1220c194_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_f0e86118_0x0_150ux0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_68321872_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_876715d1_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_bb353bd7_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_214b62f5_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_f69c1642_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_2923e1de_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_dfc3c8fe_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_660b8727_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_1203f130_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_825a75f6_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_c7b62a90_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_55b70edd_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_7d6f16e0_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_1f9697f0_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_02b9af69_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_5f5bf95b_0x5d0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_714392b0_0x0 (PLUS,MINUS);
-  output  PLUS;
-  input  MINUS;
-endmodule
-
-module vpwl_127fc54f_0x0 (PLUS,MINUS);
+module vpwl_0x0_149d999ux0_150ux5d0 (PLUS,MINUS);
   output  PLUS;
   input  MINUS;
 endmodule
@@ -611,72 +286,72 @@ STONEnoconn XNCIP_dc9fa791 (
 .noconn(noconn_IN_IP_dc9fa791)
 );
 
-vpwl_3578e779_0x0 XCELG (
+vpwl__0x0 XCELG (
 .PLUS(CELG59462),
 .MINUS(net_0)
 );
 
-vpwl_1d1997d9_0x0_10d0ux0_11d0ux5d0 XCELV (
+vpwl__0x0_10d0ux0_11d0ux5d0 XCELV (
 .PLUS(CELV96848),
 .MINUS(net_0)
 );
 
-vpwl_8dee584c_0x0_11ux0_11d001ux5d0 XPORB (
+vpwl__0x0_11ux0_11d001ux5d0 XPORB (
 .PLUS(PORB97836),
 .MINUS(net_0)
 );
 
-vpwl_97ff8a3b_0x0 XCELSUB (
+vpwl__0x0 XCELSUB (
 .PLUS(CELSUB40948),
 .MINUS(net_0)
 );
 
-vpwl_36eb942a_0x0_15ux0_15d001ux5d0 V1_80a20e20 (
+vpwl__0x0_15ux0_15d001ux5d0 V1_80a20e20 (
 .PLUS(net_71),
 .MINUS(net_0)
 );
 
-vpwl_b36cfd62_0x0_6ux0_6d001ux5d0 V3_e79154b7 (
+vpwl__0x0_6ux0_6d001ux5d0 V3_e79154b7 (
 .PLUS(SIMPV),
 .MINUS(net_0)
 );
 
-vpwl_b5c9a941_0x0_10ux0_10d001ux0 V6_203c6221 (
+vpwl__0x0_10ux0_10d001ux0 V6_203c6221 (
 .PLUS(net_77),
 .MINUS(net_0)
 );
 
-vpwl_4fa18aab_0x0_6ux0_7ux12 V8_a3c5c203 (
+vpwl__0x0_6ux0_7ux12 V8_a3c5c203 (
 .PLUS(CAP4),
 .MINUS(net_0)
 );
 
-vpwl_72a1ee49_0x0_7ux0_7d001ux5d0 V14_04447682 (
+vpwl__0x0_7ux0_7d001ux5d0 V14_04447682 (
 .PLUS(net_74),
 .MINUS(net_0)
 );
 
-vpwl_78dcda0d_0x0_6ux0_6d001ux5d0 V15_a04c0f1b (
+vpwl__0x0_6ux0_6d001ux5d0 V15_a04c0f1b (
 .PLUS(kelvin_SIMPV),
 .MINUS(net_0)
 );
 
-vpwl_4f04898e_0x0_20ux0_20d001ux5d0 V17_e030be70 (
+vpwl__0x0_20ux0_20d001ux5d0 V17_e030be70 (
 .PLUS(net_72),
 .MINUS(net_0)
 );
 
-vpwl_0acdff5b_0x0_10ux0_10d001ux0 V18_36e59b9b (
+vpwl__0x0_10ux0_10d001ux0 V18_36e59b9b (
 .PLUS(net_78),
 .MINUS(net_0)
 );
 
-vpwl_b97f08a7_0x0_6ux0_6d001ux0 V19_8b2cd8d1 (
+vpwl__0x0_6ux0_6d001ux0 V19_8b2cd8d1 (
 .PLUS(kelvin_GNDcaptest),
 .MINUS(net_0)
 );
 
-vpwl_01db9d2b_0x0_6ux0_6d001ux0 V20_463649bf (
+vpwl__0x0_6ux0_6d001ux0 V20_463649bf (
 .PLUS(GNDcaptest),
 .MINUS(net_0)
 );
@@ -702,332 +377,332 @@ vpulse_0x5d0x10d0ux10ux1nx1nx5u XV16_6d0a883a (
 .MINUS(net_0)
 );
 
-vpwl_b7bd27de_0x0_150ux0 Xdatamap1_pwl_bit0 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit0 (
 .PLUS(meas_vcap4_0),
 .MINUS(net_0)
 );
 
-vpwl_6df1465e_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit1 (
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit1 (
 .PLUS(meas_vcap4_1),
 .MINUS(net_0)
 );
 
-vpwl_d2c11a42_0x0_150ux0 Xdatamap1_pwl_bit2 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit2 (
 .PLUS(meas_vcap4_2),
 .MINUS(net_0)
 );
 
-vpwl_4d79b1f7_0x0_150ux0 Xdatamap1_pwl_bit3 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit3 (
 .PLUS(meas_vcap4_3),
 .MINUS(net_0)
 );
 
-vpwl_2302ebc2_0x0_150ux0 Xdatamap1_pwl_bit4 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit4 (
 .PLUS(meas_vcap4_4),
 .MINUS(net_0)
 );
 
-vpwl_8932f903_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit5 (
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit5 (
 .PLUS(meas_vcap4_5),
 .MINUS(net_0)
 );
 
-vpwl_2e687a9e_0x0_150ux0 Xdatamap1_pwl_bit6 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit6 (
 .PLUS(meas_vcap4_6),
 .MINUS(net_0)
 );
 
-vpwl_bb48c2c6_0x0_150ux0 Xdatamap1_pwl_bit7 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit7 (
 .PLUS(meas_vcap4_7),
 .MINUS(net_0)
 );
 
-vpwl_cfef51f0_0x0_150ux0 Xdatamap1_pwl_bit8 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit8 (
 .PLUS(meas_vcap4_8),
 .MINUS(net_0)
 );
 
-vpwl_b1459dd9_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit9 (
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit9 (
 .PLUS(meas_vcap4_9),
 .MINUS(net_0)
 );
 
-vpwl_dd874091_0x0 Xdatamap2_pwl_bit0 (
+vpwl_0x0 Xdatamap2_pwl_bit0 (
 .PLUS(meas_vcap1_0),
 .MINUS(net_0)
 );
 
-vpwl_4c73ed85_0x5d0 Xdatamap2_pwl_bit1 (
+vpwl_0x5d0 Xdatamap2_pwl_bit1 (
 .PLUS(meas_vcap1_1),
 .MINUS(net_0)
 );
 
-vpwl_ac798407_0x0 Xdatamap2_pwl_bit2 (
+vpwl_0x0 Xdatamap2_pwl_bit2 (
 .PLUS(meas_vcap1_2),
 .MINUS(net_0)
 );
 
-vpwl_a6b7f6ba_0x0 Xdatamap2_pwl_bit3 (
+vpwl_0x0 Xdatamap2_pwl_bit3 (
 .PLUS(meas_vcap1_3),
 .MINUS(net_0)
 );
 
-vpwl_b845f025_0x0 Xdatamap2_pwl_bit4 (
+vpwl_0x0 Xdatamap2_pwl_bit4 (
 .PLUS(meas_vcap1_4),
 .MINUS(net_0)
 );
 
-vpwl_749b025f_0x5d0 Xdatamap2_pwl_bit5 (
+vpwl_0x5d0 Xdatamap2_pwl_bit5 (
 .PLUS(meas_vcap1_5),
 .MINUS(net_0)
 );
 
-vpwl_e67b28ba_0x0 Xdatamap2_pwl_bit6 (
+vpwl_0x0 Xdatamap2_pwl_bit6 (
 .PLUS(meas_vcap1_6),
 .MINUS(net_0)
 );
 
-vpwl_6bf14cb7_0x0 Xdatamap2_pwl_bit7 (
+vpwl_0x0 Xdatamap2_pwl_bit7 (
 .PLUS(meas_vcap1_7),
 .MINUS(net_0)
 );
 
-vpwl_325aed86_0x0 Xdatamap2_pwl_bit8 (
+vpwl_0x0 Xdatamap2_pwl_bit8 (
 .PLUS(meas_vcap1_8),
 .MINUS(net_0)
 );
 
-vpwl_f26adafd_0x5d0 Xdatamap2_pwl_bit9 (
+vpwl_0x5d0 Xdatamap2_pwl_bit9 (
 .PLUS(meas_vcap1_9),
 .MINUS(net_0)
 );
 
-vpwl_6219f260_0x0 Xdatamap3_pwl_bit0 (
+vpwl_0x0 Xdatamap3_pwl_bit0 (
 .PLUS(meas_vcap2_0),
 .MINUS(net_0)
 );
 
-vpwl_6d3fb9d6_0x5d0 Xdatamap3_pwl_bit1 (
+vpwl_0x5d0 Xdatamap3_pwl_bit1 (
 .PLUS(meas_vcap2_1),
 .MINUS(net_0)
 );
 
-vpwl_537cdc2a_0x0 Xdatamap3_pwl_bit2 (
+vpwl_0x0 Xdatamap3_pwl_bit2 (
 .PLUS(meas_vcap2_2),
 .MINUS(net_0)
 );
 
-vpwl_ebdc8581_0x0 Xdatamap3_pwl_bit3 (
+vpwl_0x0 Xdatamap3_pwl_bit3 (
 .PLUS(meas_vcap2_3),
 .MINUS(net_0)
 );
 
-vpwl_f2738915_0x0 Xdatamap3_pwl_bit4 (
+vpwl_0x0 Xdatamap3_pwl_bit4 (
 .PLUS(meas_vcap2_4),
 .MINUS(net_0)
 );
 
-vpwl_027ae12a_0x5d0 Xdatamap3_pwl_bit5 (
+vpwl_0x5d0 Xdatamap3_pwl_bit5 (
 .PLUS(meas_vcap2_5),
 .MINUS(net_0)
 );
 
-vpwl_bd4661d6_0x0 Xdatamap3_pwl_bit6 (
+vpwl_0x0 Xdatamap3_pwl_bit6 (
 .PLUS(meas_vcap2_6),
 .MINUS(net_0)
 );
 
-vpwl_556394c1_0x0 Xdatamap3_pwl_bit7 (
+vpwl_0x0 Xdatamap3_pwl_bit7 (
 .PLUS(meas_vcap2_7),
 .MINUS(net_0)
 );
 
-vpwl_48f2086c_0x0 Xdatamap3_pwl_bit8 (
+vpwl_0x0 Xdatamap3_pwl_bit8 (
 .PLUS(meas_vcap2_8),
 .MINUS(net_0)
 );
 
-vpwl_b9d4ba9f_0x5d0 Xdatamap3_pwl_bit9 (
+vpwl_0x5d0 Xdatamap3_pwl_bit9 (
 .PLUS(meas_vcap2_9),
 .MINUS(net_0)
 );
 
-vpwl_304d6518_0x0 Xdatamap4_pwl_bit0 (
+vpwl_0x0 Xdatamap4_pwl_bit0 (
 .PLUS(meas_vcap3_0),
 .MINUS(net_0)
 );
 
-vpwl_4075232a_0x5d0 Xdatamap4_pwl_bit1 (
+vpwl_0x5d0 Xdatamap4_pwl_bit1 (
 .PLUS(meas_vcap3_1),
 .MINUS(net_0)
 );
 
-vpwl_75e85800_0x0 Xdatamap4_pwl_bit2 (
+vpwl_0x0 Xdatamap4_pwl_bit2 (
 .PLUS(meas_vcap3_2),
 .MINUS(net_0)
 );
 
-vpwl_dab7e819_0x0 Xdatamap4_pwl_bit3 (
+vpwl_0x0 Xdatamap4_pwl_bit3 (
 .PLUS(meas_vcap3_3),
 .MINUS(net_0)
 );
 
-vpwl_f1d31cca_0x0 Xdatamap4_pwl_bit4 (
+vpwl_0x0 Xdatamap4_pwl_bit4 (
 .PLUS(meas_vcap3_4),
 .MINUS(net_0)
 );
 
-vpwl_1e3f9d92_0x5d0 Xdatamap4_pwl_bit5 (
+vpwl_0x5d0 Xdatamap4_pwl_bit5 (
 .PLUS(meas_vcap3_5),
 .MINUS(net_0)
 );
 
-vpwl_3bae73ea_0x0 Xdatamap4_pwl_bit6 (
+vpwl_0x0 Xdatamap4_pwl_bit6 (
 .PLUS(meas_vcap3_6),
 .MINUS(net_0)
 );
 
-vpwl_f10feb5f_0x0 Xdatamap4_pwl_bit7 (
+vpwl_0x0 Xdatamap4_pwl_bit7 (
 .PLUS(meas_vcap3_7),
 .MINUS(net_0)
 );
 
-vpwl_63779624_0x0 Xdatamap4_pwl_bit8 (
+vpwl_0x0 Xdatamap4_pwl_bit8 (
 .PLUS(meas_vcap3_8),
 .MINUS(net_0)
 );
 
-vpwl_125717fe_0x5d0 Xdatamap4_pwl_bit9 (
+vpwl_0x5d0 Xdatamap4_pwl_bit9 (
 .PLUS(meas_vcap3_9),
 .MINUS(net_0)
 );
 
-vpwl_eed155ff_0x5d0 Xdatamap5_pwl_bit0 (
+vpwl_0x5d0 Xdatamap5_pwl_bit0 (
 .PLUS(CAPcount_0),
 .MINUS(net_0)
 );
 
-vpwl_a9867611_0x5d0 Xdatamap5_pwl_bit1 (
+vpwl_0x5d0 Xdatamap5_pwl_bit1 (
 .PLUS(CAPcount_1),
 .MINUS(net_0)
 );
 
-vpwl_afd00360_0x0_150ux0 Xdatamap1_pwl_bit10 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit10 (
 .PLUS(meas_vcap4_10),
 .MINUS(net_0)
 );
 
-vpwl_62f58745_0x0_150ux0 Xdatamap1_pwl_bit11 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit11 (
 .PLUS(meas_vcap4_11),
 .MINUS(net_0)
 );
 
-vpwl_76288a8e_0x0_149d999ux0_150ux5d0 Xdatamap1_pwl_bit12 (
+vpwl_0x0_149d999ux0_150ux5d0 Xdatamap1_pwl_bit12 (
 .PLUS(meas_vcap4_12),
 .MINUS(net_0)
 );
 
-vpwl_7139535e_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit13 (
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit13 (
 .PLUS(meas_vcap4_13),
 .MINUS(net_0)
 );
 
-vpwl_1220c194_0x0_150ux0 Xdatamap1_pwl_bit14 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit14 (
 .PLUS(meas_vcap4_14),
 .MINUS(net_0)
 );
 
-vpwl_f0e86118_0x0_150ux0 Xdatamap1_pwl_bit15 (
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit15 (
 .PLUS(meas_vcap4_15),
 .MINUS(net_0)
 );
 
-vpwl_68321872_0x0 Xdatamap2_pwl_bit10 (
+vpwl_0x0 Xdatamap2_pwl_bit10 (
 .PLUS(meas_vcap1_10),
 .MINUS(net_0)
 );
 
-vpwl_876715d1_0x0 Xdatamap2_pwl_bit11 (
+vpwl_0x0 Xdatamap2_pwl_bit11 (
 .PLUS(meas_vcap1_11),
 .MINUS(net_0)
 );
 
-vpwl_bb353bd7_0x0 Xdatamap2_pwl_bit12 (
+vpwl_0x0 Xdatamap2_pwl_bit12 (
 .PLUS(meas_vcap1_12),
 .MINUS(net_0)
 );
 
-vpwl_214b62f5_0x5d0 Xdatamap2_pwl_bit13 (
+vpwl_0x5d0 Xdatamap2_pwl_bit13 (
 .PLUS(meas_vcap1_13),
 .MINUS(net_0)
 );
 
-vpwl_f69c1642_0x0 Xdatamap2_pwl_bit14 (
+vpwl_0x0 Xdatamap2_pwl_bit14 (
 .PLUS(meas_vcap1_14),
 .MINUS(net_0)
 );
 
-vpwl_2923e1de_0x0 Xdatamap2_pwl_bit15 (
+vpwl_0x0 Xdatamap2_pwl_bit15 (
 .PLUS(meas_vcap1_15),
 .MINUS(net_0)
 );
 
-vpwl_dfc3c8fe_0x0 Xdatamap3_pwl_bit10 (
+vpwl_0x0 Xdatamap3_pwl_bit10 (
 .PLUS(meas_vcap2_10),
 .MINUS(net_0)
 );
 
-vpwl_660b8727_0x0 Xdatamap3_pwl_bit11 (
+vpwl_0x0 Xdatamap3_pwl_bit11 (
 .PLUS(meas_vcap2_11),
 .MINUS(net_0)
 );
 
-vpwl_1203f130_0x0 Xdatamap3_pwl_bit12 (
+vpwl_0x0 Xdatamap3_pwl_bit12 (
 .PLUS(meas_vcap2_12),
 .MINUS(net_0)
 );
 
-vpwl_825a75f6_0x5d0 Xdatamap3_pwl_bit13 (
+vpwl_0x5d0 Xdatamap3_pwl_bit13 (
 .PLUS(meas_vcap2_13),
 .MINUS(net_0)
 );
 
-vpwl_c7b62a90_0x0 Xdatamap3_pwl_bit14 (
+vpwl_0x0 Xdatamap3_pwl_bit14 (
 .PLUS(meas_vcap2_14),
 .MINUS(net_0)
 );
 
-vpwl_55b70edd_0x0 Xdatamap3_pwl_bit15 (
+vpwl_0x0 Xdatamap3_pwl_bit15 (
 .PLUS(meas_vcap2_15),
 .MINUS(net_0)
 );
 
-vpwl_7d6f16e0_0x0 Xdatamap4_pwl_bit10 (
+vpwl_0x0 Xdatamap4_pwl_bit10 (
 .PLUS(meas_vcap3_10),
 .MINUS(net_0)
 );
 
-vpwl_1f9697f0_0x0 Xdatamap4_pwl_bit11 (
+vpwl_0x0 Xdatamap4_pwl_bit11 (
 .PLUS(meas_vcap3_11),
 .MINUS(net_0)
 );
 
-vpwl_02b9af69_0x0 Xdatamap4_pwl_bit12 (
+vpwl_0x0 Xdatamap4_pwl_bit12 (
 .PLUS(meas_vcap3_12),
 .MINUS(net_0)
 );
 
-vpwl_5f5bf95b_0x5d0 Xdatamap4_pwl_bit13 (
+vpwl_0x5d0 Xdatamap4_pwl_bit13 (
 .PLUS(meas_vcap3_13),
 .MINUS(net_0)
 );
 
-vpwl_714392b0_0x0 Xdatamap4_pwl_bit14 (
+vpwl_0x0 Xdatamap4_pwl_bit14 (
 .PLUS(meas_vcap3_14),
 .MINUS(net_0)
 );
 
-vpwl_127fc54f_0x0 Xdatamap4_pwl_bit15 (
+vpwl_0x0 Xdatamap4_pwl_bit15 (
 .PLUS(meas_vcap3_15),
 .MINUS(net_0)
 );
