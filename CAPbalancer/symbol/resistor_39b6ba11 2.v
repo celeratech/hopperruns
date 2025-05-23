a8c2ec0ac6edd77a564182ec55b5b6a7f025e3d9
Hirshol Pheir <hirshol@gmail.com>
2025-05-23 16:04:14 +0000

    hopper run REGULATIONcomparator_20250523.16.04.14

---
+++ CAPbalancer/symbol/resistor_39b6ba11 2.v
@@ -0,0 +1,11 @@
+//Celera:resistor_39b6ba11
+//Celera Confidential Symbol Generator
+//RESISTOR:0.18000KOhm TYPE:rpodrpo DFT:no
+module resistor_39b6ba11 (RP,
+CELPOS,
+RN);
+inout RP;
+inout RN;
+input CELPOS;
+endmodule
+
---
+++ CAPtest/verilog11/FORCEcaptest 2.v
@@ -0,0 +1,715 @@
+// ------------------------ Module Definitions -----------
+//Celera:dbuf_e926e395
+//Celera Confidential Symbol Generator
+//Digital Buffer
+module dbuf_e926e395 (CELV,CELG,i,o,SUB);
+input CELV;
+input CELG;
+input i;
+input SUB;
+output o;
+endmodule
+
+
+
+//Verilog HDL for "Generate", "STONEnoconn" "functional"
+
+
+module STONEnoconn ( noconn );
+
+  input noconn;
+endmodule
+
+
+module vpwl__0x0(PLUS, MINUS);
+	output PLUS;
+	input MINUS;
+endmodule
+
+
+module vpwl__0x0_10d0ux0_11d0ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_11ux0_11d001ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_15ux0_15d001ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_6ux0_6d001ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_10ux0_10d001ux0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_6ux0_7ux12 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_7ux0_7d001ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_20ux0_20d001ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl__0x0_6ux0_6d001ux0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+//Verilog HDL for "TEST", "SIMbias" "functional"
+
+
+module SIMbias ( V, IP, IN );
+
+  input V;
+  output IN;
+  output IP;
+endmodule
+
+
+module vpulse_0x5d0x50d0ux30ux50nx50nx1d95u(PLUS, MINUS);
+	output PLUS;
+	input MINUS;
+endmodule
+
+module vpulse_0x5d0x1d0ux10ux1nx1nx500n(PLUS, MINUS);
+	output PLUS;
+	input MINUS;
+endmodule
+
+module vpulse_0x5d0x10d0ux10ux1nx1nx5u (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl_0x0_150ux0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl_0x5d0_149d999ux5d0_150ux0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl_0x0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl_0x5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module vpwl_0x0_149d999ux0_150ux5d0 (PLUS,MINUS);
+  output  PLUS;
+  input  MINUS;
+endmodule
+
+module alibgnd (GND); 
+input GND; 
+endmodule 
+
+// ------------------------ Module Verilog ---------------
+module FORCEcaptest (CAP4, porb, SIMPV, CELG59462, CELV96848, PORB97836, CAPcount_0, CAPcount_1, GNDcaptest, go_captest, CELSUB40948, IP_dc9fa791, kelvin_SIMPV, meas_vcap1_0, meas_vcap1_1, meas_vcap1_2, meas_vcap1_3, meas_vcap1_4, meas_vcap1_5, meas_vcap1_6, meas_vcap1_7, meas_vcap1_8, meas_vcap1_9, meas_vcap2_0, meas_vcap2_1, meas_vcap2_2, meas_vcap2_3, meas_vcap2_4, meas_vcap2_5, meas_vcap2_6, meas_vcap2_7, meas_vcap2_8, meas_vcap2_9, meas_vcap3_0, meas_vcap3_1, meas_vcap3_2, meas_vcap3_3, meas_vcap3_4, meas_vcap3_5, meas_vcap3_6, meas_vcap3_7, meas_vcap3_8, meas_vcap3_9, meas_vcap4_0, meas_vcap4_1, meas_vcap4_2, meas_vcap4_3, meas_vcap4_4, meas_vcap4_5, meas_vcap4_6, meas_vcap4_7, meas_vcap4_8, meas_vcap4_9, clock_captest, ctl_cap_scale, meas_vcap1_10, meas_vcap1_11, meas_vcap1_12, meas_vcap1_13, meas_vcap1_14, meas_vcap1_15, meas_vcap2_10, meas_vcap2_11, meas_vcap2_12, meas_vcap2_13, meas_vcap2_14, meas_vcap2_15, meas_vcap3_10, meas_vcap3_11, meas_vcap3_12, meas_vcap3_13, meas_vcap3_14, meas_vcap3_15, meas_vcap4_10, meas_vcap4_11, meas_vcap4_12, meas_vcap4_13, meas_vcap4_14, meas_vcap4_15, clock_divideRT, enable_captest, request_clockRT, done_capcalculate, kelvin_GNDcaptest);
+output  CAP4;
+output  porb;
+output  SIMPV;
+output  CELG59462;
+output  CELV96848;
+output  PORB97836;
+output  CAPcount_0;
+output  CAPcount_1;
+output  GNDcaptest;
+output  go_captest;
+output  CELSUB40948;
+output  IP_dc9fa791;
+output  kelvin_SIMPV;
+output  meas_vcap1_0;
+output  meas_vcap1_1;
+output  meas_vcap1_2;
+output  meas_vcap1_3;
+output  meas_vcap1_4;
+output  meas_vcap1_5;
+output  meas_vcap1_6;
+output  meas_vcap1_7;
+output  meas_vcap1_8;
+output  meas_vcap1_9;
+output  meas_vcap2_0;
+output  meas_vcap2_1;
+output  meas_vcap2_2;
+output  meas_vcap2_3;
+output  meas_vcap2_4;
+output  meas_vcap2_5;
+output  meas_vcap2_6;
+output  meas_vcap2_7;
+output  meas_vcap2_8;
+output  meas_vcap2_9;
+output  meas_vcap3_0;
+output  meas_vcap3_1;
+output  meas_vcap3_2;
+output  meas_vcap3_3;
+output  meas_vcap3_4;
+output  meas_vcap3_5;
+output  meas_vcap3_6;
+output  meas_vcap3_7;
+output  meas_vcap3_8;
+output  meas_vcap3_9;
+output  meas_vcap4_0;
+output  meas_vcap4_1;
+output  meas_vcap4_2;
+output  meas_vcap4_3;
+output  meas_vcap4_4;
+output  meas_vcap4_5;
+output  meas_vcap4_6;
+output  meas_vcap4_7;
+output  meas_vcap4_8;
+output  meas_vcap4_9;
+output  clock_captest;
+output  ctl_cap_scale;
+output  meas_vcap1_10;
+output  meas_vcap1_11;
+output  meas_vcap1_12;
+output  meas_vcap1_13;
+output  meas_vcap1_14;
+output  meas_vcap1_15;
+output  meas_vcap2_10;
+output  meas_vcap2_11;
+output  meas_vcap2_12;
+output  meas_vcap2_13;
+output  meas_vcap2_14;
+output  meas_vcap2_15;
+output  meas_vcap3_10;
+output  meas_vcap3_11;
+output  meas_vcap3_12;
+output  meas_vcap3_13;
+output  meas_vcap3_14;
+output  meas_vcap3_15;
+output  meas_vcap4_10;
+output  meas_vcap4_11;
+output  meas_vcap4_12;
+output  meas_vcap4_13;
+output  meas_vcap4_14;
+output  meas_vcap4_15;
+output  clock_divideRT;
+output  enable_captest;
+output  request_clockRT;
+output  done_capcalculate;
+output  kelvin_GNDcaptest;
+
+
+// ------------------------ Wires ------------------------
+
+// ------------------------ Networks ---------------------
+dbuf_e926e395 XU2 (
+.i(net_74),
+.o(porb),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU4 (
+.i(net_77),
+.o(request_clockRT),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU11 (
+.i(net_75),
+.o(clock_captest),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU13 (
+.i(net_73),
+.o(done_capcalculate),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU17 (
+.i(net_76),
+.o(clock_divideRT),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU18 (
+.i(net_72),
+.o(go_captest),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU19 (
+.i(net_78),
+.o(ctl_cap_scale),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+dbuf_e926e395 XU35 (
+.i(net_71),
+.o(enable_captest),
+.SUB(CELSUB40948),
+.CELG(CELG59462),
+.CELV(CELV96848)
+);
+
+STONEnoconn XNCIP_dc9fa791 (
+.noconn(noconn_IN_IP_dc9fa791)
+);
+
+vpwl__0x0 XCELG (
+.PLUS(CELG59462),
+.MINUS(net_0)
+);
+
+vpwl__0x0_10d0ux0_11d0ux5d0 XCELV (
+.PLUS(CELV96848),
+.MINUS(net_0)
+);
+
+vpwl__0x0_11ux0_11d001ux5d0 XPORB (
+.PLUS(PORB97836),
+.MINUS(net_0)
+);
+
+vpwl__0x0 XCELSUB (
+.PLUS(CELSUB40948),
+.MINUS(net_0)
+);
+
+vpwl__0x0_15ux0_15d001ux5d0 V1_80a20e20 (
+.PLUS(net_71),
+.MINUS(net_0)
+);
+
+vpwl__0x0_6ux0_6d001ux5d0 V3_e79154b7 (
+.PLUS(SIMPV),
+.MINUS(net_0)
+);
+
+vpwl__0x0_10ux0_10d001ux0 V6_203c6221 (
+.PLUS(net_77),
+.MINUS(net_0)
+);
+
+vpwl__0x0_6ux0_7ux12 V8_a3c5c203 (
+.PLUS(CAP4),
+.MINUS(net_0)
+);
+
+vpwl__0x0_7ux0_7d001ux5d0 V14_04447682 (
+.PLUS(net_74),
+.MINUS(net_0)
+);
+
+vpwl__0x0_6ux0_6d001ux5d0 V15_a04c0f1b (
+.PLUS(kelvin_SIMPV),
+.MINUS(net_0)
+);
+
+vpwl__0x0_20ux0_20d001ux5d0 V17_e030be70 (
+.PLUS(net_72),
+.MINUS(net_0)
+);
+
+vpwl__0x0_10ux0_10d001ux0 V18_36e59b9b (
+.PLUS(net_78),
+.MINUS(net_0)
+);
+
+vpwl__0x0_6ux0_6d001ux0 V19_8b2cd8d1 (
+.PLUS(kelvin_GNDcaptest),
+.MINUS(net_0)
+);
+
+vpwl__0x0_6ux0_6d001ux0 V20_463649bf (
+.PLUS(GNDcaptest),
+.MINUS(net_0)
+);
+
+SIMbias XIP_dc9fa791 (
+.V(SIMPV),
+.IN(noconn_IN_IP_dc9fa791),
+.IP(IP_dc9fa791)
+);
+
+vpulse_0x5d0x50d0ux30ux50nx50nx1d95u XV12_9b1dff8a (
+.PLUS(net_73),
+.MINUS(net_0)
+);
+
+vpulse_0x5d0x1d0ux10ux1nx1nx500n XV13_1a603c9f (
+.PLUS(net_75),
+.MINUS(net_0)
+);
+
+vpulse_0x5d0x10d0ux10ux1nx1nx5u XV16_6d0a883a (
+.PLUS(net_76),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit0 (
+.PLUS(meas_vcap4_0),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit1 (
+.PLUS(meas_vcap4_1),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit2 (
+.PLUS(meas_vcap4_2),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit3 (
+.PLUS(meas_vcap4_3),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit4 (
+.PLUS(meas_vcap4_4),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit5 (
+.PLUS(meas_vcap4_5),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit6 (
+.PLUS(meas_vcap4_6),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit7 (
+.PLUS(meas_vcap4_7),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit8 (
+.PLUS(meas_vcap4_8),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit9 (
+.PLUS(meas_vcap4_9),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit0 (
+.PLUS(meas_vcap1_0),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap2_pwl_bit1 (
+.PLUS(meas_vcap1_1),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit2 (
+.PLUS(meas_vcap1_2),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit3 (
+.PLUS(meas_vcap1_3),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit4 (
+.PLUS(meas_vcap1_4),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap2_pwl_bit5 (
+.PLUS(meas_vcap1_5),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit6 (
+.PLUS(meas_vcap1_6),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit7 (
+.PLUS(meas_vcap1_7),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit8 (
+.PLUS(meas_vcap1_8),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap2_pwl_bit9 (
+.PLUS(meas_vcap1_9),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit0 (
+.PLUS(meas_vcap2_0),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap3_pwl_bit1 (
+.PLUS(meas_vcap2_1),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit2 (
+.PLUS(meas_vcap2_2),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit3 (
+.PLUS(meas_vcap2_3),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit4 (
+.PLUS(meas_vcap2_4),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap3_pwl_bit5 (
+.PLUS(meas_vcap2_5),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit6 (
+.PLUS(meas_vcap2_6),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit7 (
+.PLUS(meas_vcap2_7),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit8 (
+.PLUS(meas_vcap2_8),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap3_pwl_bit9 (
+.PLUS(meas_vcap2_9),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit0 (
+.PLUS(meas_vcap3_0),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap4_pwl_bit1 (
+.PLUS(meas_vcap3_1),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit2 (
+.PLUS(meas_vcap3_2),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit3 (
+.PLUS(meas_vcap3_3),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit4 (
+.PLUS(meas_vcap3_4),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap4_pwl_bit5 (
+.PLUS(meas_vcap3_5),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit6 (
+.PLUS(meas_vcap3_6),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit7 (
+.PLUS(meas_vcap3_7),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit8 (
+.PLUS(meas_vcap3_8),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap4_pwl_bit9 (
+.PLUS(meas_vcap3_9),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap5_pwl_bit0 (
+.PLUS(CAPcount_0),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap5_pwl_bit1 (
+.PLUS(CAPcount_1),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit10 (
+.PLUS(meas_vcap4_10),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit11 (
+.PLUS(meas_vcap4_11),
+.MINUS(net_0)
+);
+
+vpwl_0x0_149d999ux0_150ux5d0 Xdatamap1_pwl_bit12 (
+.PLUS(meas_vcap4_12),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0_149d999ux5d0_150ux0 Xdatamap1_pwl_bit13 (
+.PLUS(meas_vcap4_13),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit14 (
+.PLUS(meas_vcap4_14),
+.MINUS(net_0)
+);
+
+vpwl_0x0_150ux0 Xdatamap1_pwl_bit15 (
+.PLUS(meas_vcap4_15),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit10 (
+.PLUS(meas_vcap1_10),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit11 (
+.PLUS(meas_vcap1_11),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit12 (
+.PLUS(meas_vcap1_12),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap2_pwl_bit13 (
+.PLUS(meas_vcap1_13),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit14 (
+.PLUS(meas_vcap1_14),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap2_pwl_bit15 (
+.PLUS(meas_vcap1_15),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit10 (
+.PLUS(meas_vcap2_10),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit11 (
+.PLUS(meas_vcap2_11),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit12 (
+.PLUS(meas_vcap2_12),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap3_pwl_bit13 (
+.PLUS(meas_vcap2_13),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit14 (
+.PLUS(meas_vcap2_14),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap3_pwl_bit15 (
+.PLUS(meas_vcap2_15),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit10 (
+.PLUS(meas_vcap3_10),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit11 (
+.PLUS(meas_vcap3_11),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit12 (
+.PLUS(meas_vcap3_12),
+.MINUS(net_0)
+);
+
+vpwl_0x5d0 Xdatamap4_pwl_bit13 (
+.PLUS(meas_vcap3_13),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit14 (
+.PLUS(meas_vcap3_14),
+.MINUS(net_0)
+);
+
+vpwl_0x0 Xdatamap4_pwl_bit15 (
+.PLUS(meas_vcap3_15),
+.MINUS(net_0)
+);
+
+alibgnd G1 ( 
+.GND(net_0) 
+); 
+
+endmodule
+
