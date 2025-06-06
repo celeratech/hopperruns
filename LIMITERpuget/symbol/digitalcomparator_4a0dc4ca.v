6fbfc156d4a88261463bb6ea05d7f6ccfa913ed7
Hirshol Pheir <hirshol@gmail.com>
2025-06-06 17:50:46 +0000

    hopper run CAPbalancer_20250606.17.50.46

---
+++ LIMITERpuget/symbol/digitalcomparator_4a0dc4ca.v
@@ -0,0 +1,14 @@
+//Celera:digitalcomparator_4a0dc4ca
+//Celera Confidential Symbol Generator
+//Number of inputs: 16
+//DFT:no
+module digitalcomparator_4a0dc4ca (CELV,SUB,x,y,xgty,
+CELG);
+input CELV;
+input CELG;
+input SUB;
+output xgty;
+input [15:0] x;
+input [15:0] y;
+endmodule
+
