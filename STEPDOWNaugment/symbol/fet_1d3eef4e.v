2f72a435cea87fcabe2ee82f221100eed3840338
Hirshol Pheir <hirshol@gmail.com>
2025-07-28 21:00:51 +0000

    hopper run STEPDOWNaugment_20250728.21.00.51

---
+++ STEPDOWNaugment/symbol/fet_1d3eef4e.v
@@ -0,0 +1,16 @@
+//Celera:fet_1d3eef4e
+//Celera Confidential Symbol Generator
+//power NMOS:Ron:0.100 Ohm
+//Vgs 6V Vds 30V
+//Kelvin:yes
+
+module fet_1d3eef4e (GATE,SOURCE,DRAIN,SOURCEk,DRAINk,IREPLICA,SUB);
+input GATE;
+inout SOURCE;
+inout DRAIN;
+inout DRAINk;
+inout SOURCEk;
+input SUB;
+inout IREPLICA;
+endmodule
+
