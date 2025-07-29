d9bffc9952df18360e336c59bc850cb846c602db
Hirshol Pheir <hirshol@gmail.com>
2025-07-29 15:19:12 +0000

    hopper run TELEMETRYmeasureVIN_20250729.15.19.12

--- TELEMETRYmeasureVIN/verilog10/sim_TELEMETRYmeasureVIN_ROW9.v
+++ TELEMETRYmeasureVIN/verilog10/sim_TELEMETRYmeasureVIN_ROW9.v
@@ -22,6 +22,15 @@ module TELEMETRYmeasureVIN (TIN,ok_vin,CELG59462,CELV96848,kelvin_IN,enable_vin,
   output  dftprobe_XUINstartup_5be9f382;
 endmodule
 
+//Verilog HDL for "Generate", "STONEnoconn" "functional"
+
+
+module STONEnoconn ( noconn );
+
+  input noconn;
+endmodule
+
+
 // ------------------------ Module Verilog ---------------
 module sim_TELEMETRYmeasureVIN_ROW9 ();
 
@@ -50,6 +59,14 @@ TELEMETRYmeasureVIN XTELEMETRYmeasureVIN1 (
 .kelvin_GNDtelemetry(kelvin_GNDtelemetry),
 .dftprobe_XUTIN_5be9f382(dftprobe_XUTIN_5be9f382),
 .dftprobe_XUINstartup_5be9f382(dftprobe_XUINstartup_5be9f382)
+);
+
+STONEnoconn XNCdftprobe_XUTIN_5be9f382 (
+.noconn(dftprobe_XUTIN_5be9f382)
+);
+
+STONEnoconn XNCdftprobe_XUINstartup_5be9f382 (
+.noconn(dftprobe_XUINstartup_5be9f382)
 );
 
 endmodule
